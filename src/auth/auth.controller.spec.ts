61180479e98caf3be8c404a9967a750526a62455
Michał Garapich <michal@garapich.pl>
2019-12-10 01:43:23 +0100

    fix AuthController test

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
+import { ConfigService } from '@/config/config.service';
+import { HttpAdapterHost } from '@nestjs/core';
+import { AuthService } from './auth.service';
+
+class ConfigServiceStub {
+  clientUrl = '';
+}
+
+class HttpAdapterHostStub {
+  httpAdapter = {
+    get: (route: string, cb: any) => null,
+  };
+}
+
+class AuthServiceStub {
+  generateJwtToken(name: string, userId: string) { return ''; }
+}
 
 describe('Auth Controller', () => {
   let controller: AuthController;
@@ -7,6 +24,11 @@ describe('Auth Controller', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [AuthController],
+      providers: [
+        { provide: ConfigService, useClass: ConfigServiceStub },
+        { provide: HttpAdapterHost, useClass: HttpAdapterHostStub },
+        { provide: AuthService, useClass: AuthServiceStub },
+      ],
     }).compile();
 
     controller = module.get<AuthController>(AuthController);
