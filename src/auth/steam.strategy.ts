48dee12f9780881fb85600ca92a02e619e9966db
Michał Garapich <michal@garapich.pl>
2019-12-10 04:19:20 +0100

    SteamStrategy cleanup

--- src/auth/steam.strategy.ts
+++ src/auth/steam.strategy.ts
@@ -1,41 +1,36 @@
 import { Injectable } from '@nestjs/common';
-import { use } from 'passport';
 import steam = require('passport-steam');
 import { PlayersService } from 'src/players/players.service';
 import { ConfigService } from 'src/config/config.service';
-import { Player } from 'src/players/models/player';
 import { SteamProfile } from 'src/players/models/steam-profile';
+import { PassportStrategy } from '@nestjs/passport';
 
 @Injectable()
-export class SteamStrategy {
+export class SteamStrategy extends PassportStrategy(steam.Strategy) {
 
   constructor(
-    private configService: ConfigService,
+    configService: ConfigService,
     private playerService: PlayersService,
   ) {
-    this.init();
+    super({
+      returnURL: `${configService.apiUrl}/auth/steam/return`,
+      realm: configService.apiUrl,
+      apiKey: configService.steamApiKey,
+    });
+
+    // go to AuthController for the steam return route
   }
 
-  private init() {
-    use(new steam.Strategy({
-      returnURL: `${this.configService.apiUrl}/auth/steam/return`,
-      realm: this.configService.apiUrl,
-      apiKey: this.configService.steamApiKey,
-    }, async (identifier: any, profile: SteamProfile, done: (error: any, player: Player) => void) => {
-      const player = await this.playerService.findBySteamId(profile.id);
-      if (player) {
-        player.avatarUrl = profile.photos[0].value;
-        await player.save();
-        return done(null, player);
-      } else {
-        try {
-          const newPlayer = await this.playerService.createPlayer(profile);
-          return done(null, newPlayer);
-        } catch (error) {
-          return done(error, null);
-        }
-      }
-    }));
+  async validate(identifier: any, profile: SteamProfile) {
+    const player = await this.playerService.findBySteamId(profile.id);
+    if (player) {
+      player.avatarUrl = profile.photos[0].value;
+      await player.save();
+      return player.toJSON();
+    } else {
+      const newPlayer = await this.playerService.createPlayer(profile);
+      return newPlayer.toJSON();
+    }
   }
 
 }
