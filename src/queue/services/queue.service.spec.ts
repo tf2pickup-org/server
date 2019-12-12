1e118bb2b4e606a01c00cc91cdd921fc11b6b166
Michał Garapich <michal@garapich.pl>
2019-12-12 04:08:25 +0100

    QueueService update

--- src/queue/services/queue.service.spec.ts
+++ src/queue/services/queue.service.spec.ts
@@ -3,6 +3,7 @@ import { QueueService } from './queue.service';
 import { PlayersService } from '@/players/services/players.service';
 import { Player } from '@/players/models/player';
 import { QueueConfigService } from './queue-config.service';
+import { PlayerBansService } from '@/players/services/player-bans.service';
 
 class PlayersServiceStub {
 
@@ -36,10 +37,17 @@ class QueueConfigServiceStub {
   };
 }
 
+class PlayerBansServiceStub {
+  getActiveBansForPlayer(playerId: string) {
+    return new Promise(resolve => resolve([]));
+  }
+}
+
 describe('QueueService', () => {
   let service: QueueService;
   let playersService: PlayersServiceStub;
   let queueConfigService: QueueConfigServiceStub;
+  let playerBansService: PlayerBansServiceStub;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -47,12 +55,14 @@ describe('QueueService', () => {
         QueueService,
         { provide: PlayersService, useClass: PlayersServiceStub },
         { provide: QueueConfigService, useClass: QueueConfigServiceStub },
+        { provide: PlayerBansService, useClass: PlayerBansServiceStub },
       ],
     }).compile();
 
     service = module.get<QueueService>(QueueService);
     playersService = module.get(PlayersService);
     queueConfigService = module.get(QueueConfigService);
+    playerBansService = module.get(PlayerBansService);
   });
 
   it('should be defined', () => {
@@ -64,18 +74,24 @@ describe('QueueService', () => {
     expect(service.slots.length).toBe(12);
     expect(service.slots.every(s => s.playerId === null)).toBe(true);
     expect(service.slots.every(s => s.ready === false)).toBe(true);
-    // expect(service.playerCount).toEqual(0);
+    expect(service.playerCount).toEqual(0);
     // expect(service.readyPlayerCount).toEqual(0);
   });
 
   describe('#join()', () => {
-    xit('should fail if the queue is in launching state', () => {
-      // todo
-    });
-
     it('should fail if the given player doesn\'t exist', async () => {
       spyOn(playersService, 'getById').and.returnValue(new Promise(resolve => resolve(null)));
       await expectAsync(service.join(0, 'FAKE_PLAYER_ID')).toBeRejectedWithError('no such player');
+    });
+
+    it('should fail if the joining player hasn\'t accepted rules', async () => {
+      spyOn(playersService, 'getById').and.returnValue(new Promise(resolve => resolve({ ...playersService.player, hasAcceptedRules: false })));
+      await expectAsync(service.join(0, 'FAKE_ID')).toBeRejectedWithError('player has not accepted rules');
+    });
+
+    it('should fail if the player is banned', async () => {
+      spyOn(playerBansService, 'getActiveBansForPlayer').and.returnValue(new Promise(resolve => resolve([{}])));
+      await expectAsync(service.join(0, 'FAKE_ID')).toBeRejectedWithError('player is banned');
     });
 
     it('should fail when trying to join an ivalid slot', async () => {
@@ -90,7 +106,51 @@ describe('QueueService', () => {
     it('should store add the player to the given slot', async () => {
       const slots = await service.join(0, 'FAKE_PLAYER_ID');
       const slot = slots.find(s => s.playerId === 'FAKE_PLAYER_ID');
+      expect(slot.id).toBe(0);
       expect(slot.playerId).toEqual('FAKE_PLAYER_ID');
+      expect(service.playerCount).toBe(1);
+    });
+
+    it('should remove the player from already taken slot', async () => {
+      const oldSlots = await service.join(0, 'FAKE_PLAYER_ID');
+      const newSlots = await service.join(1, 'FAKE_PLAYER_ID');
+      expect(newSlots.length).toEqual(2);
+      expect(newSlots.find(s => s.playerId === 'FAKE_PLAYER_ID')).toBeTruthy();
+      expect(oldSlots[0].playerId).toBeNull();
+    });
+  });
+
+  describe('#leave()', () => {
+    beforeEach(async () => {
+      await service.join(0, 'FAKE_PLAYER_ID');
+    });
+
+    it('should reset the slot', async () => {
+      const slot = await service.leave('FAKE_PLAYER_ID');
+      expect(slot.id).toBe(0);
+      expect(slot.playerId).toBe(null);
+      expect(slot.ready).toBe(false);
+    });
+  });
+
+  describe('#kick()', () => {
+    beforeEach(async () => {
+      await service.join(0, 'FAKE_PLAYER_ID');
+    });
+
+    it('should reset the slot', () => {
+      service.kick('FAKE_PLAYER_ID');
+      const slot = service.getSlotById(0);
+      expect(slot.playerId).toBe(null);
+      expect(slot.ready).toBe(false);
+      expect(service.playerCount).toBe(0);
+    });
+  });
+
+  describe('#readyUp()', () => {
+    it('should fail if the queue is not in ready up state', async () => {
+      await service.join(0, 'FAKE_PLAYER_ID');
+      expect(() => service.readyUp('FAKE_PLAYER_ID')).toThrowError('queue not ready');
     });
   });
 });
--- src/queue/services/queue.service.ts
+++ src/queue/services/queue.service.ts
@@ -3,6 +3,8 @@ import { QueueSlot } from '@/queue/queue-slot';
 import { PlayersService } from '@/players/services/players.service';
 import { QueueConfigService } from './queue-config.service';
 import { PlayerBansService } from '@/players/services/player-bans.service';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { pairwise, distinctUntilChanged } from 'rxjs/operators';
 
 // waiting: waiting for players
 // ready: players are expected to ready up
@@ -13,18 +15,41 @@ type QueueState = 'waiting' | 'ready' | 'launching';
 export class QueueService {
 
   slots: QueueSlot[] = [];
-  private state: QueueState = 'waiting';
+  private _state = new BehaviorSubject<QueueState>('waiting');
+  private timer: NodeJS.Timer;
+
+  get requiredPlayerCount(): number {
+    return this.slots.length;
+  }
+
+  get playerCount(): number {
+    return this.slots.filter(s => !!s.playerId).length;
+  }
+
+  get readyPlayerCount() {
+    return this.slots.filter(s => s.ready).length;
+  }
+
+  get state(): Observable<QueueState> {
+    return this._state.asObservable();
+  }
 
   constructor(
     private playersService: PlayersService,
     private queueConfigService: QueueConfigService,
     private playerBansService: PlayerBansService,
   ) {
+    this.state.pipe(
+      distinctUntilChanged(),
+      pairwise(),
+    ).subscribe(([oldState, newState]) => this.onStateChange(oldState, newState));
+
     this.resetSlots();
   }
 
   reset() {
     this.resetSlots();
+    setImmediate(() => this.maybeUpdateState());
   }
 
   /**
@@ -34,13 +59,22 @@ export class QueueService {
    * @param {string} playerId ID of the player who joins the queue.
    */
   async join(slotId: number, playerId: string): Promise<QueueSlot[]> {
-    if (this.state === 'launching') {
+    if (this._state.value === 'launching') {
       throw new Error('cannot join the queue at this stage');
     }
 
     const player = await this.playersService.getById(playerId);
     if (!player) {
       throw new Error('no such player');
+    }
+
+    if (!player.hasAcceptedRules) {
+      throw new Error('player has not accepted rules');
+    }
+
+    const bans = await this.playerBansService.getActiveBansForPlayer(playerId);
+    if (bans.length > 0) {
+      throw new Error('player is banned');
     }
 
     const targetSlot = this.slots.find(s => s.id === slotId);
@@ -57,11 +91,60 @@ export class QueueService {
     oldSlots.forEach(s => this.clearSlot(s));
 
     targetSlot.playerId = playerId;
-    if (this.state === 'ready') {
+    if (this._state.value === 'ready') {
       targetSlot.ready = true;
     }
 
+    setImmediate(() => this.maybeUpdateState());
     return [ targetSlot, ...oldSlots ];
+  }
+
+  async leave(playerId: string): Promise<QueueSlot> {
+    const slot = this.slots.find(s => s.playerId === playerId);
+    if (slot) {
+      if (slot.ready && this._state.value !== 'waiting') {
+        throw new Error('cannot leave at this stage');
+      }
+
+      this.clearSlot(slot);
+      setImmediate(() => this.maybeUpdateState());
+      return slot;
+    } else {
+      return null;
+    }
+  }
+
+  kick(...playerIds: string[]) {
+    if (this._state.value === 'launching') {
+      return;
+    }
+
+    const slots = this.slots.filter(s => playerIds.includes(s.playerId));
+    slots.forEach(s => this.clearSlot(s));
+    setImmediate(() => this.maybeUpdateState());
+  }
+
+  readyUp(playerId: string): QueueSlot {
+    if (this._state.value !== 'ready') {
+      throw new Error('queue not ready');
+    }
+
+    const slot = this.slots.find(s => s.playerId === playerId);
+    if (slot) {
+      slot.ready = true;
+      setImmediate(() => this.maybeUpdateState());
+      return slot;
+    } else {
+      throw new Error('player is not in the queue');
+    }
+  }
+
+  getSlotById(id: number): QueueSlot {
+    return this.slots.find(s => s.id === id);
+  }
+
+  isInQueue(playerId: string): boolean {
+    return !!this.slots.find(s => s.playerId === playerId);
   }
 
   private resetSlots() {
@@ -88,5 +171,63 @@ export class QueueService {
   private clearSlot(slot: QueueSlot) {
     slot.playerId = null;
   }
+
+  private maybeUpdateState() {
+    // check whether we can change state
+    switch (this._state.value) {
+      case 'waiting':
+        if (this.playerCount === this.requiredPlayerCount) {
+          this._state.next('ready');
+        }
+        break;
+
+      case 'ready':
+        if (this.playerCount === 0) {
+          this._state.next('waiting');
+        } else if (this.readyPlayerCount === this.requiredPlayerCount) {
+          this._state.next('launching');
+        }
+        break;
+
+      case 'launching':
+        this._state.next('waiting');
+        break;
+    }
+  }
+
+  private onStateChange(oldState: QueueState, newState: QueueState) {
+    if (oldState === 'waiting' && newState === 'ready') {
+      this.timer = setTimeout(() => this.readyUpTimeout(), this.queueConfigService.queueConfig.readyUpTimeout);
+    } else if (oldState === 'ready' && newState === 'launching') {
+      clearTimeout(this.timer);
+    }
+  }
+
+  private readyUpTimeout() {
+    if (this.readyPlayerCount < this.requiredPlayerCount) {
+      this.kickUnreadyPlayers();
+    }
+
+    const nextTimeout =
+      this.queueConfigService.queueConfig.queueReadyTimeout - this.queueConfigService.queueConfig.readyUpTimeout;
+
+    if (nextTimeout > 0) {
+      setTimeout(() => this.unreadyQueue(), nextTimeout);
+    } else {
+      this.unreadyQueue();
+    }
+  }
+
+  private kickUnreadyPlayers() {
+    const slots = this.slots.filter(s => !s.ready);
+    this.kick(...slots.map(s => s.playerId));
+  }
+
+  private unreadyQueue() {
+    const slots = this.slots.filter(s => s.ready);
+    slots.forEach(s => s.ready = false);
+    this._state.next('waiting');
+  }
+
 
 }
