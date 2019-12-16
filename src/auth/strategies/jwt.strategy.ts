5b3acee5594f57a3a0f5583f06cbba94469a4530
Michał Garapich <michal@garapich.pl>
2019-12-16 18:32:02 +0100

    auth module fixup

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -20,7 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 
   async validate(payload: { id: string }) {
     const player = await this.playersService.getById(payload.id);
-    return player.toJSON(); // retrieve this via the @User() decorator
+    return player?.toJSON(); // retrieve this via the @User() decorator
   }
 
 }
