a0f92601f7337b08adccb0f80cdd086bf82ce7e0
Michał Garapich <michal@garapich.pl>
2019-12-28 19:48:24 +0100

    add ObjectIdValidationPipe tests

--- src/shared/pipes/object-id-validation.pipe.spec.ts
+++ src/shared/pipes/object-id-validation.pipe.spec.ts
@@ -1,7 +1,17 @@
 import { ObjectIdValidationPipe } from './object-id-validation.pipe';
+import { ObjectId } from 'mongodb';
 
 describe('ObjectIdValidationPipe', () => {
   it('should be defined', () => {
     expect(new ObjectIdValidationPipe()).toBeDefined();
+  });
+
+  it('should pass valid object id', () => {
+    const id = new ObjectId().toString();
+    expect(new ObjectIdValidationPipe().transform(id, null)).toEqual(id);
+  });
+
+  it('should deny invalid object id',() => {
+    expect(() => new ObjectIdValidationPipe().transform('some invalid id', null)).toThrow();
   });
 });
