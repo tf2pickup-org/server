a890d803c568c17dd8fc70594901d2ddcefcf63a
Michał Garapich <michal@garapich.pl>
2019-12-28 19:40:37 +0100

    GameServersService: update tests

--- src/game-servers/services/game-servers.service.spec.ts
+++ src/game-servers/services/game-servers.service.spec.ts
@@ -1,29 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameServersService } from './game-servers.service';
-import { getModelToken } from 'nestjs-typegoose';
+import { TypegooseModule, getModelToken } from 'nestjs-typegoose';
 import { GameServer } from '../models/game-server';
-import { DocumentType } from '@typegoose/typegoose';
-
-const gameServerModel = {
-  find: (obj?: any) => new Promise(resolve => resolve(null)),
-  findById: (id: string) => new Promise(resolve => resolve(null)),
-  findOne: (obj: any) => new Promise(resolve => resolve(null)),
-  deleteOne: (obj: any) => new Promise(resolve => resolve({ ok: true })),
-};
+import { DocumentType, ReturnModelType } from '@typegoose/typegoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { ObjectId } from 'mongodb';
 
 describe('GameServersService', () => {
+  const mongod = new MongoMemoryServer();
   let service: GameServersService;
+  let gameServerModel: ReturnModelType<typeof GameServer>;
+  let testGameServer: DocumentType<GameServer>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypegooseModule.forRootAsync({
+          useFactory: async () => ({
+            uri: await mongod.getConnectionString(),
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          }),
+        }),
+        TypegooseModule.forFeature([GameServer]),
+      ],
       providers: [
         GameServersService,
-        { provide: getModelToken('GameServer'), useValue: gameServerModel },
       ],
     }).compile();
 
     service = module.get<GameServersService>(GameServersService);
+    gameServerModel = module.get(getModelToken('GameServer'));
   });
+
+  beforeEach(async () => {
+    testGameServer = await gameServerModel.create({
+      name: 'TEST_GAME_SERVER',
+      address: 'localhost',
+      port: '27015',
+      rconPassword: '123456',
+      resolvedIpAddresses: [ '127.0.0.1' ],
+    });
+  });
+
+  afterEach(async () => await gameServerModel.deleteMany({ }));
 
   it('should be defined', () => {
     expect(service).toBeDefined();
@@ -31,77 +51,74 @@ describe('GameServersService', () => {
 
   describe('#getAllServers()', () => {
     it('should query model', async () => {
-      const spy = spyOn(gameServerModel, 'find').and.callThrough();
-      await service.getAllGameServers();
-      expect(spy).toHaveBeenCalledWith();
+      const ret = await service.getAllGameServers();
+      expect(ret.length).toBe(1);
+      expect(ret[0].toJSON()).toEqual(testGameServer.toJSON());
     });
   });
 
   describe('#getById()', () => {
     it('should query model', async () => {
-      const spy = spyOn(gameServerModel, 'findById').and.callThrough();
-      await service.getById('FAKE_ID');
-      expect(spy).toHaveBeenCalledWith('FAKE_ID');
+      const ret = await service.getById(testGameServer.id);
+      expect(ret.toJSON()).toEqual(testGameServer.toJSON());
     });
   });
 
   describe('#removeGameServer()', () => {
-    it('should query model', async () => {
-      const spy = spyOn(gameServerModel, 'deleteOne').and.callThrough();
-      await service.removeGameServer('FAKE_ID');
-      expect(spy).toHaveBeenCalledWith({ _id: 'FAKE_ID' });
+    it('should delete the given game server', async () => {
+      await service.removeGameServer(testGameServer.id);
+      expect(await gameServerModel.countDocuments()).toBe(0);
     });
 
     it('should fail gracefully', async () => {
-      spyOn(gameServerModel, 'deleteOne').and.returnValue(new Promise(resolve => resolve({ ok: false })));
-      await expectAsync(service.removeGameServer('FAKE_ID')).toBeRejectedWithError('unable to remove game server');
+      spyOn(gameServerModel, 'deleteOne').and.returnValue(new Promise(resolve => resolve({ ok: false })) as any);
+      await expectAsync(service.removeGameServer(testGameServer.id)).toBeRejectedWithError('unable to remove game server');
     });
   });
 
   describe('#findFreeGameServer()', () => {
-    it('should query model', async () => {
-      const spy = spyOn(gameServerModel, 'findOne').and.callThrough();
-      await service.findFreeGameServer();
-      expect(spy).toHaveBeenCalledWith({ isOnline: true, isFree: true });
+    it('should return the server that is both online and free', async () => {
+      testGameServer.isFree = true;
+      testGameServer.isOnline = true;
+      await testGameServer.save();
+
+      const goodServer = await service.findFreeGameServer();
+      expect(goodServer.toJSON()).toEqual(testGameServer.toJSON());
+
+      testGameServer.isFree = false;
+      testGameServer.isOnline = true;
+      await testGameServer.save();
+      expect(await service.findFreeGameServer()).toBeNull();
+
+      testGameServer.isFree = true;
+      testGameServer.isOnline = false;
+      await testGameServer.save();
+      expect(await service.findFreeGameServer()).toBeNull();
     });
   });
 
   describe('#takeServer()', () => {
-    const gameServer = {
-      isFree: true,
-      name: 'FAKE_GAME_SERVER_NAME',
-      save: () => null,
-    };
-
     it('should set isFree property to false and save', async () => {
-      const getByIdSpy = spyOn(service, 'getById').and.returnValue(new Promise(resolve => resolve(gameServer as DocumentType<GameServer>)));
-      const saveSpy = spyOn(gameServer, 'save');
-      await service.takeServer('FAKE_ID');
-      expect(getByIdSpy).toHaveBeenCalledWith('FAKE_ID');
-      expect(gameServer.isFree).toBe(false);
-      expect(saveSpy).toHaveBeenCalled();
+      await service.takeServer(testGameServer.id);
+      expect((await service.getById(testGameServer.id)).isFree).toBe(false);
     });
 
     it('should fail gracefully', async () => {
-      spyOn(service, 'getById').and.returnValue(new Promise(resolve => resolve(null)));
-      await expectAsync(service.takeServer('FAKE_ID')).toBeRejectedWithError('no such game server');
+      await expectAsync(service.takeServer(new ObjectId().toString())).toBeRejectedWithError('no such game server');
     });
   });
 
   describe('#releaseServer()', () => {
-    const gameServer = {
-      isFree: false,
-      name: 'FAKE_GAME_SERVER_NAME',
-      save: () => null,
-    };
-
     it('should set isFree property to true and save', async () => {
-      const getByIdSpy = spyOn(service, 'getById').and.returnValue(new Promise(resolve => resolve(gameServer as DocumentType<GameServer>)));
-      const saveSpy = spyOn(gameServer, 'save');
-      await service.releaseServer('FAKE_ID');
-      expect(getByIdSpy).toHaveBeenCalledWith('FAKE_ID');
-      expect(gameServer.isFree).toBe(true);
-      expect(saveSpy).toHaveBeenCalled();
+      await service.releaseServer(testGameServer.id);
+      expect((await service.getById(testGameServer.id)).isFree).toBe(true);
+    });
+  });
+
+  describe('#getGameServerByEventSource()', () => {
+    it('should return the correct server', async () => {
+      const server = await service.getGameServerByEventSource({ address: '127.0.0.1', port: 27015 });
+      expect(server.toJSON()).toEqual(testGameServer.toJSON());
     });
   });
 });
