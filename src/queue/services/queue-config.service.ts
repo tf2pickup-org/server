086214908032b4acb99d14f7e5fa21c607689129
Michał Garapich <michal@garapich.pl>
2019-12-31 01:33:19 +0100

    validate queue config agains schema

--- src/queue/services/queue-config.service.ts
+++ src/queue/services/queue-config.service.ts
@@ -3,6 +3,7 @@ import { QueueConfig } from '../queue-config';
 import { Environment } from '@/environment/environment';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import { Validator, Schema } from 'jsonschema';
 
 @Injectable()
 export class QueueConfigService {
@@ -18,5 +19,75 @@ export class QueueConfigService {
     this.logger.log(`Using ${configFileName} for queue config`);
     const data = readFileSync(configFileName, 'utf-8');
     this.queueConfig = JSON.parse(data) as QueueConfig;
+    this.validateQueueConfig(this.queueConfig);
+  }
+
+  private validateQueueConfig(data: any) {
+    const validator = new Validator();
+
+    const teamCountSchema: Schema = {
+      id: '/teamCount',
+      type: 'integer',
+      minimum: 2,
+      maximum: 2,
+    };
+    validator.addSchema(teamCountSchema);
+
+    const gameClassSchema: Schema = {
+      id: '/gameClass',
+      type: 'object',
+      properties: {
+        name: {
+          enum: [
+            'scout',
+            'soldier',
+            'pyro',
+            'demoman',
+            'heavy',
+            'engineer',
+            'medic',
+            'sniper',
+            'spy',
+          ],
+        },
+        count: {
+          type: 'integer',
+          minimum: 1,
+        },
+      },
+    };
+    validator.addSchema(gameClassSchema);
+
+    const queueConfigSchema: Schema = {
+      id: '/queueConfig',
+      type: 'object',
+      properties: {
+        teamCount: {
+          $ref: '/teamCount',
+        },
+        classes: {
+          type: 'array',
+          items: {
+            $ref: '/gameClass',
+          },
+        },
+        maps: {
+          type: 'array',
+          items:  {
+            type: 'string',
+          },
+          minItems: 0,
+        },
+        execConfigs: {
+          type: 'array',
+          items: {
+            type: 'string',
+          },
+        },
+      },
+      additionalItems: false,
+    };
+
+    validator.validate(data, queueConfigSchema, { throwError: true });
   }
 }
