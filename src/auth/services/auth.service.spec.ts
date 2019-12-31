5bcac2d807f54310ad66d30cfa8852ee00a74e37
Michał <michal@garapich.pl>
2019-12-31 15:08:00 +0100

    tests update

--- src/auth/services/auth.service.spec.ts
+++ src/auth/services/auth.service.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { KeyStoreService } from './key-store.service';
-import { getModelToken } from 'nestjs-typegoose';
+import { getModelToken, TypegooseModule } from 'nestjs-typegoose';
 import { generateKeyPairSync } from 'crypto';
-import { decode } from 'jsonwebtoken';
+import { decode, sign, verify } from 'jsonwebtoken';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import { RefreshToken } from '../models/refresh-token';
+import { ReturnModelType } from '@typegoose/typegoose';
 
 class KeyStoreServiceStub {
   public key = generateKeyPairSync('ec', {
@@ -26,26 +29,36 @@ class KeyStoreServiceStub {
   }
 }
 
-const refreshTokenModel = {
-  create: (object: any) => null,
-};
-
 describe('AuthService', () => {
+  const mongod = new MongoMemoryServer();
   let service: AuthService;
   let keyStoreService: KeyStoreServiceStub;
+  let refreshTokenModel: ReturnModelType<typeof RefreshToken>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [
+        TypegooseModule.forRootAsync({
+          useFactory: async () => ({
+            uri: await mongod.getConnectionString(),
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+          }),
+        }),
+        TypegooseModule.forFeature([RefreshToken]),
+      ],
       providers: [
         AuthService,
         { provide: KeyStoreService, useClass: KeyStoreServiceStub },
-        { provide: getModelToken('RefreshToken'), useValue: refreshTokenModel },
       ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
     keyStoreService = module.get(KeyStoreService);
+    refreshTokenModel = module.get(getModelToken('RefreshToken'));
   });
+
+  afterEach(async () => await refreshTokenModel.deleteMany({ }));
 
   it('should be defined', () => {
     expect(service).toBeDefined();
@@ -53,48 +66,48 @@ describe('AuthService', () => {
 
   describe('#generateJwtToken()', () => {
     describe('auth', () => {
-      it('should retrieve the signing auth key from the key store', () => {
+      it('should retrieve the signing auth key from the key store', async () => {
         const spy = spyOn(keyStoreService, 'getKey').and.callThrough();
-        service.generateJwtToken('auth', 'FAKE_USER_ID');
+        await service.generateJwtToken('auth', 'FAKE_USER_ID');
         expect(spy).toHaveBeenCalledWith('auth', 'sign');
       });
 
-      it('should encode user id', () => {
-        const token = service.generateJwtToken('auth', 'FAKE_USER_ID');
+      it('should encode user id', async () => {
+        const token = await service.generateJwtToken('auth', 'FAKE_USER_ID');
         const decoded = decode(token) as { id: string; iat: number; exp: number };
         expect(decoded.id).toEqual('FAKE_USER_ID');
       });
     });
 
     describe('refresh', () => {
-      it('should retrieve the signing refresh key from the key store', () => {
+      it('should retrieve the signing refresh key from the key store', async () => {
         const spy = spyOn(keyStoreService, 'getKey').and.callThrough();
-        service.generateJwtToken('refresh', 'FAKE_USER_ID');
+        await service.generateJwtToken('refresh', 'FAKE_USER_ID');
         expect(spy).toHaveBeenCalledWith('refresh', 'sign');
       });
 
-      it('should encode user id', () => {
-        const token = service.generateJwtToken('refresh', 'FAKE_USER_ID');
+      it('should encode user id', async () => {
+        const token = await service.generateJwtToken('refresh', 'FAKE_USER_ID');
         const decoded = decode(token) as { id: string; iat: number; exp: number };
         expect(decoded.id).toEqual('FAKE_USER_ID');
       });
 
-      it('should store the token in the database', () => {
-        const spy = spyOn(refreshTokenModel, 'create').and.callThrough();
-        const value = service.generateJwtToken('refresh', 'FAKE_USER_ID');
-        expect(spy).toHaveBeenCalledWith({ value });
+      it('should store the token in the database', async () => {
+        const value = await service.generateJwtToken('refresh', 'FAKE_USER_ID');
+        const key = await refreshTokenModel.findOne({ value });
+        expect(key).toBeDefined();
       });
     });
 
     describe('ws', () => {
-      it('should retrieve the ws secret from the key store', () => {
+      it('should retrieve the ws secret from the key store', async () => {
         const spy = spyOn(keyStoreService, 'getKey').and.callThrough();
-        service.generateJwtToken('ws', 'FAKE_USER_ID');
+        await service.generateJwtToken('ws', 'FAKE_USER_ID');
         expect(spy).toHaveBeenCalledWith('ws', jasmine.any(String));
       });
 
-      it('should encode user id', () => {
-        const token = service.generateJwtToken('ws', 'FAKE_USER_ID');
+      it('should encode user id', async () => {
+        const token = await service.generateJwtToken('ws', 'FAKE_USER_ID');
         const decoded = decode(token) as { id: string, iat: number, exp: number };
         expect(decoded.id).toEqual('FAKE_USER_ID');
       });
@@ -102,6 +115,61 @@ describe('AuthService', () => {
   });
 
   describe('#refreshTokens()', () => {
-    // todo
+    it('should throw an error if the refresh token is not in the database', async () => {
+      await expectAsync(service.refreshTokens('some fake token')).toBeRejectedWithError('invalid token');
+    });
+
+    it('should throw an error if the refresh token has expired', async () => {
+      const key = keyStoreService.getKey('refresh', 'sign');
+
+      // issue a token that has already expired
+      const oneWeekAgo = new Date();
+      oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+      const token = sign({ id: 'FAKE_USER_ID', iat: Math.floor(oneWeekAgo.getDate() / 1000) - 60 }, key, { algorithm: 'ES512', expiresIn: '7d' });
+      await refreshTokenModel.create({ value: token });
+
+      await expectAsync(service.refreshTokens(token)).toBeRejectedWithError('jwt expired');
+    });
+
+    it('should throw an error unless the refresh token matches', async () => {
+      const key = generateKeyPairSync('ec', { namedCurve: 'secp521r1' });
+      const token = sign({ id: 'FAKE_USER_ID' }, key.privateKey.export({ format: 'pem', type: 'pkcs8' }), { algorithm: 'ES512', expiresIn: '7d' });
+      await refreshTokenModel.create({ value: token });
+
+      await expectAsync(service.refreshTokens(token)).toBeRejectedWithError('invalid signature');
+    });
+
+    it('should generate auth and refresh tokens', async () => {
+      const oldRefreshToken = await service.generateJwtToken('refresh', 'FAKE_USER_ID');
+      const { refreshToken, authToken } = await service.refreshTokens(oldRefreshToken);
+
+      const key = keyStoreService.getKey('refresh', 'verify');
+
+      const refreshTokenDecoded = verify(refreshToken, key, { algorithms: ['ES512'] }) as { id: string; iat: number; exp: number };
+      expect(refreshTokenDecoded.id).toEqual('FAKE_USER_ID');
+
+      const authTokenDecoded = verify(authToken, key, { algorithms: ['ES512'] }) as { id: string, iat: number, exp: number };
+      expect(authTokenDecoded.id).toEqual('FAKE_USER_ID');
+    });
+
+    it('should remove the old refresh token', async () => {
+      const oldRefreshToken = await service.generateJwtToken('refresh', 'FAKE_USER_ID');
+      await service.refreshTokens(oldRefreshToken);
+      expect(await refreshTokenModel.findOne({ value: oldRefreshToken })).toBeNull();
+    });
+  });
+
+  describe('#removeOldRefreshTokens()', () => {
+    it('should remove old tokens', async () => {
+      const key = keyStoreService.getKey('refresh', 'sign');
+      const token = sign({ id: 'FAKE_USER_ID' }, key, this.refreshTokenOptions);
+
+      const createdAt = new Date();
+      createdAt.setDate(createdAt.getDate() - 8);
+      await refreshTokenModel.create({ value: token, createdAt });
+
+      await service.removeOldRefreshTokens();
+      expect(await refreshTokenModel.findOne({ value: token })).toBeNull();
+    });
   });
 });
