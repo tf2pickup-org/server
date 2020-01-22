6403ae2dd06f7d0a446fda7f7ebde8461c0c278d
Michał Garapich <michal@garapich.pl>
2020-01-23 00:12:53 +0100

    setup logging levels

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import { LogLevel } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  let logLevels: LogLevel[];
+
+  if (process.env.NODE_ENV === 'production') {
+    logLevels = ['error', 'warn', 'log', 'verbose'];
+  } else {
+    logLevels = ['error', 'warn', 'log', 'verbose', 'debug'];
+  }
+
+  const app = await NestFactory.create(AppModule, {
+    logger: logLevels,
+  });
   app.enableCors();
   app.use(helmet());
   await app.listen(3000);
