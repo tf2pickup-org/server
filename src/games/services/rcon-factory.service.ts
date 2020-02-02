2d9622cf48abdb203149c298ea9ac95ce4700e6d
Michał Garapich <michal@garapich.pl>
2020-02-02 23:59:23 +0100

    RconFactoryService: handle connect() errors

--- src/games/services/rcon-factory.service.ts
+++ src/games/services/rcon-factory.service.ts
@@ -6,14 +6,20 @@ import { Rcon } from 'rcon-client';
 export class RconFactoryService {
 
   async createRcon(gameServer: GameServer): Promise<Rcon> {
-    const rcon = new Rcon({
-      host: gameServer.address,
-      port: parseInt(gameServer.port, 10),
-      password: gameServer.rconPassword,
-      timeout: 30000,
+    return new Promise(async  (resolve, reject) => {
+      const rcon = new Rcon({
+        host: gameServer.address,
+        port: parseInt(gameServer.port, 10),
+        password: gameServer.rconPassword,
+        timeout: 30000,
+      });
+
+      rcon.on('error', error => {
+        return reject(error);
+      });
+
+      rcon.connect().then(resolve).catch(reject);
     });
-    await rcon.connect();
-    return rcon;
   }
 
 }
