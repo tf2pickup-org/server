b1a18498d80f86978e897c91f0fcd864e4525ea8
Michał Garapich <michal@garapich.pl>
2020-02-21 17:35:29 +0100

    DiscordNotificationsService test initial

--- src/discord/services/discord-notifications.service.spec.ts
+++ src/discord/services/discord-notifications.service.spec.ts
@@ -3,16 +3,37 @@ import { DiscordNotificationsService } from './discord-notifications.service';
 import { Environment } from '@/environment/environment';
 import { PlayersService } from '@/players/services/players.service';
 import { ConfigService } from '@nestjs/config';
+import { Client, Channel, Collection, TextChannel } from 'discord.js';
 
-class EnvironmentStub { }
+class EnvironmentStub {
+  discordBotToken = 'FAKE_DISCORD_BOT_TOKEN';
+  discordQueueNotificationsChannel = 'queue';
+}
+
 class PlayersServiceStub { }
 
 class ConfigServiceStub {
-  get(key: string) { return null; }
+  get(key: string) {
+    switch (key) {
+      case 'discordNotifications.promptJoinQueueMentionRole':
+        return 'FAKE_ROLE';
+
+      default:
+        return null;
+    }
+  }
 }
+
+jest.mock('discord.js');
 
 describe('DiscordNotificationsService', () => {
   let service: DiscordNotificationsService;
+  let client: Client;
+  let queueChannel: TextChannel;
+
+  beforeEach(() => {
+    (Client as any as jest.MockedClass<typeof Client>).mockClear();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,7 +48,33 @@ describe('DiscordNotificationsService', () => {
     service = module.get<DiscordNotificationsService>(DiscordNotificationsService);
   });
 
+  beforeEach(() => {
+    client = (Client as any as jest.MockedClass<typeof Client>).mock.instances[0];
+
+    client.user = { tag: 'bot#1337' } as any;
+
+    jest.spyOn(client, 'login').mockResolvedValue('FAKE_DISCORD_BOT_TOKEN');
+    jest.spyOn(client, 'on').mockImplementation((event, listener) => {
+      if (event === 'ready') {
+        listener();
+      }
+      return client;
+    });
+  });
+
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create the Client instance', () => {
+    expect(Client).toHaveBeenCalledTimes(1);
+  });
+
+  describe('#onModuleInit()', () => {
+    it('should login', () => {
+      const spy = jest.spyOn(client, 'login');
+      service.onModuleInit();
+      expect(spy).toHaveBeenCalledWith('FAKE_DISCORD_BOT_TOKEN');
+    });
   });
 });
