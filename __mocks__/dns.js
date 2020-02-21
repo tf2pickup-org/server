0e325e0607e29ad2c69135c7aa29dde476ff6609
Michał Garapich <michal@garapich.pl>
2020-02-21 15:19:21 +0100

    add dns mock

---
+++ __mocks__/dns.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const dns = jest.genMockFromModule('dns');
+
+function resolve(hostname, callback) {
+    callback(null, ['1.2.3.4']);
+}
+
+dns.resolve = resolve;
+
+module.exports = dns;
