8dbb9a1531f4855463344825b885118e5eaa2fc8
Michał Garapich <michal@garapich.pl>
2020-05-14 00:18:56 +0200

    update DiscordNotificationsService tests

---
+++ __mocks__/discord.js.ts
@@ -0,0 +1,63 @@
+import { EventEmitter } from 'events';
+import { Collection } from '../node_modules/discord.js/src';
+
+export class Message { }
+
+export class TextChannel {
+  constructor(public name: string) { }
+  send(message: string) { return Promise.resolve(new Message()); }
+}
+
+export const queueChannel = new TextChannel('queue');
+export const adminChannel = new TextChannel('admins');
+
+export class Role {
+  constructor(public name: string) { }
+
+  mentionable = true;
+
+  toString() {
+    return `&<${this.name}>`;
+  }
+}
+
+export const pickupsRole = new Role('pickups');
+
+export class Guild {
+  constructor(public name: string) { }
+
+  available = true;
+
+  channels = {
+    cache: new Collection([
+      [ 'queue', queueChannel ],
+      [ 'admins', adminChannel ],
+    ]),
+  };
+
+  roles = {
+    cache: new Collection([
+      [ 'pickups', pickupsRole ],
+    ]),
+  };
+}
+
+export class Client extends EventEmitter {
+
+  static _instance: Client;
+
+  user = { tag: 'bot#1337' };
+  guilds = {
+    cache: new Collection([ [ 'guild1', new Guild('FAKE_GUILD') ] ]),
+  }
+
+  constructor() {
+    super();
+    Client._instance = this;
+  }
+
+  login(token: string) { return Promise.resolve('FAKE_TOKEN'); }
+
+}
+
+export { MessageEmbed } from '../node_modules/discord.js/src';
--- src/discord/services/discord-notifications.service.spec.ts
+++ src/discord/services/discord-notifications.service.spec.ts
@@ -1,22 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { DiscordNotificationsService } from './discord-notifications.service';
+import { DiscordNotificationsService, TargetChannel } from './discord-notifications.service';
 import { Environment } from '@/environment/environment';
 import { ConfigService } from '@nestjs/config';
-import { Client, TextChannel } from 'discord.js';
 import { MessageEmbedFactoryService } from './message-embed-factory.service';
 import { PlayersService } from '@/players/services/players.service';
+import { Client, queueChannel, pickupsRole, adminChannel } from '@mocks/discord.js';
+import { MessageEmbed } from 'discord.js';
+import { ObjectId } from 'mongodb';
 
 class EnvironmentStub {
   discordBotToken = 'FAKE_DISCORD_BOT_TOKEN';
-  discordQueueNotificationsChannel = 'queue';
-}
+  discordGuild = 'FAKE_GUILD';
+  discordQueueNotificationsMentionRole = pickupsRole.name;
+  discordQueueNotificationsChannel = queueChannel.name;
+  discordAdminNotificationsChannel = adminChannel.name;
+};
 
 class ConfigServiceStub {
   get(key: string) {
     switch (key) {
-      case 'discordNotifications.promptJoinQueueMentionRole':
-        return 'FAKE_ROLE';
-
       default:
         return null;
     }
@@ -24,18 +26,13 @@ class ConfigServiceStub {
 }
 
 class PlayersServiceStub {
-
+  getById() { return Promise.resolve({ name: 'FAKE_PLAYER' }); }
 }
-
-jest.mock('discord.js');
 
 describe('DiscordNotificationsService', () => {
   let service: DiscordNotificationsService;
+  let environment: EnvironmentStub;
   let client: Client;
-
-  beforeEach(() => {
-    (Client as any as jest.MockedClass<typeof Client>).mockClear();
-  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -49,33 +46,19 @@ describe('DiscordNotificationsService', () => {
     }).compile();
 
     service = module.get<DiscordNotificationsService>(DiscordNotificationsService);
-  });
-
-  beforeEach(() => {
-    client = (Client as any as jest.MockedClass<typeof Client>).mock.instances[0];
-    client.user = { tag: 'bot#1337' } as any;
-
-    jest.spyOn(client, 'login').mockResolvedValue('FAKE_DISCORD_BOT_TOKEN');
-    jest.spyOn(client, 'on').mockImplementation((event, listener) => {
-      if (event === 'ready') {
-        listener();
-      }
-      return client;
-    });
+    environment = module.get(Environment);
+    client = Client._instance;
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it('should create the Client instance', () => {
-    expect(Client).toHaveBeenCalledTimes(1);
-  });
-
   describe('#onModuleInit()', () => {
     it('should login', () => {
       const spy = jest.spyOn(client, 'login');
       service.onModuleInit();
+      client.emit('ready');
       expect(spy).toHaveBeenCalledWith('FAKE_DISCORD_BOT_TOKEN');
     });
   });
@@ -83,10 +66,134 @@ describe('DiscordNotificationsService', () => {
   describe('when logged in', () => {
     beforeEach(() => {
       service.onModuleInit();
+      client.emit('ready');
     });
 
     describe('#notifyQueue()', () => {
+      describe('when the role to mention exists and is mentionable', () => {
+        it('should send the message mentioning the role', () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          service.notifyQueue(6, 12);
+          expect(spy).toHaveBeenCalledWith(expect.stringMatching('&<pickups> 6/12'));
+        });
+      });
 
+      describe('when the role to mention exists but is not mentionable', () => {
+        beforeEach(() => {
+          pickupsRole.mentionable = false;
+        });
+
+        it('should send the message without mentioning the role', () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          service.notifyQueue(6, 12);
+          expect(spy).toHaveBeenCalledWith(expect.stringMatching('6/12'));
+        });
+      });
+
+      describe('when the role to mention does not exist', () => {
+        beforeEach(() => {
+          environment.discordQueueNotificationsMentionRole = 'foo';
+        });
+
+        it('should send the message without mentioning the role', () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          service.notifyQueue(6, 12);
+          expect(spy).toHaveBeenCalledWith(expect.stringMatching('6/12'));
+        });
+      });
+
+      describe('when the channel does not exist', () => {
+        beforeEach(() => {
+          environment.discordQueueNotificationsChannel = 'foo';
+        });
+
+        it('should not send any messages', () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          service.notifyQueue(6, 12);
+          expect(spy).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('#sendNotification()', () => {
+      let notification: MessageEmbed;
+
+      beforeEach(() => {
+        notification = new MessageEmbed().setTitle('test');
+      });
+
+      describe('when the channel exists', () => {
+        it('should send a notification to the queue channel', async () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          await service.sendNotification(TargetChannel.Queue, notification);
+          expect(spy).toHaveBeenCalledWith(notification);
+        });
+
+        it('should send a notification to the admins channel', async () => {
+          const spy = jest.spyOn(adminChannel, 'send');
+          await service.sendNotification(TargetChannel.Admins, notification);
+          expect(spy).toHaveBeenCalledWith(notification);
+        });
+      });
+
+      describe('when the channel does not exist', () => {
+        beforeEach(() => {
+          environment.discordQueueNotificationsChannel = 'foo';
+          environment.discordAdminNotificationsChannel = 'bar';
+        });
+
+        it('should not send anything', async () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          await service.sendNotification(TargetChannel.Queue, notification);
+          expect(spy).not.toHaveBeenCalled();
+        });
+      });
+
+      describe('when the channel is not specified', () => {
+        beforeEach(() => {
+          delete environment.discordQueueNotificationsChannel;
+        });
+
+        it('should not send anything', async () => {
+          const spy = jest.spyOn(queueChannel, 'send');
+          await service.sendNotification(TargetChannel.Queue, notification);
+          expect(spy).not.toHaveBeenCalled();
+        });
+      });
+    });
+
+    describe('#notifySubstituteRequest()', () => {
+      it('should send a notification to the queue channel', async () => {
+        const spy = jest.spyOn(queueChannel, 'send');
+        await service.notifySubstituteRequest({ gameId: 'FAKE_GAME', gameNumber: 3, gameClass: 'soldier', team: 'RED' });
+        expect(spy).toHaveBeenCalledWith(expect.any(MessageEmbed));
+      });
+    });
+
+    describe('#notifyPlayerBanAdded()', () => {
+      it('should send a notification to the queue channel', async () => {
+        const spy = jest.spyOn(adminChannel, 'send');
+        await service.notifyPlayerBanAdded({ player: new ObjectId(), admin: new ObjectId(), start: new Date(),
+            end: new Date(), _id: new ObjectId().toString() });
+        expect(spy).toHaveBeenCalledWith(expect.any(MessageEmbed));
+      });
+    });
+
+    describe('#notifyPlayerBanRevoked()', () => {
+      it('should send a notification to the queue channel', async () => {
+        const spy = jest.spyOn(adminChannel, 'send');
+        await service.notifyPlayerBanRevoked({ player: new ObjectId(), admin: new ObjectId(), start: new Date(),
+            end: new Date(), _id: new ObjectId().toString() });
+        expect(spy).toHaveBeenCalledWith(expect.any(MessageEmbed));
+      });
+    });
+
+    describe('#notifyNewPlayer()', () => {
+      it('should send a notification to the queue channel', async () => {
+        const spy = jest.spyOn(adminChannel, 'send');
+        await service.notifyNewPlayer({ id: 'FAKE_PLAYER_ID', name: 'FAKE_PLAYER', steamId: 'FAKE_STEAM_ID', hasAcceptedRules: true });
+        expect(spy).toHaveBeenCalledWith(expect.any(MessageEmbed));
+      });
     });
   });
 });
