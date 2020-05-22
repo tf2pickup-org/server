80d7e1143b5d0fb7f86f37655d503fa6130b564f
Michał Garapich <michal@garapich.pl>
2020-05-22 13:56:04 +0200

    fix: remove TwitchGateway namespace (#391)
    * TwitchGateway: remove namespace
    * update event name

--- src/twitch/gateways/twitch.gateway.ts
+++ src/twitch/gateways/twitch.gateway.ts
@@ -2,13 +2,13 @@ import { WebSocketGateway, OnGatewayInit } from '@nestjs/websockets';
 import { Socket } from 'socket.io';
 import { TwitchStream } from '../models/twitch-stream';
 
-@WebSocketGateway({ namespace: 'twitch' })
+@WebSocketGateway()
 export class TwitchGateway implements OnGatewayInit {
 
   private socket: Socket;
 
   emitStreamsUpdate(streams: TwitchStream[]) {
-    this.socket?.emit('streams update', streams)
+    this.socket?.emit('twitch streams update', streams)
   }
 
   afterInit(socket: Socket) {
