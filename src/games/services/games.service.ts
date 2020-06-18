15780b779153b1fad714970b74815ab876d23f25
Michał Garapich <michal@garapich.pl>
2020-06-18 02:03:25 +0200

    fix: fix hall of fame reporting (#445)

--- src/games/services/games.service.ts
+++ src/games/services/games.service.ts
@@ -134,11 +134,11 @@ export class GamesService {
   async getMostActivePlayers() {
     return this.gameModel.aggregate([
       { $match: { state: 'ended' } },
-      { $unwind: '$players' },
-      { $group: { _id: '$players', count: { $sum: 1 } } },
+      { $unwind: '$slots' },
+      { $group: { _id: '$slots.player', count: { $sum: 1 } } },
       { $sort: { count: -1 } },
       { $limit: 10 },
-      { $project: { player: '$_id', count: 1, _id: 0 } },
+      { $project: { player: { $toString: '$_id' }, count: 1, _id: 0 } },
     ]);
   }
 
@@ -147,7 +147,7 @@ export class GamesService {
       { $match: { state: 'ended' } },
       { $unwind: '$slots' },
       { $match: { 'slots.gameClass': 'medic' } },
-      { $group: { _id: '$slots.playerId', count: { $sum: 1 } } },
+      { $group: { _id: '$slots.player', count: { $sum: 1 } } },
       { $sort: { count: -1 } },
       { $limit: 10 },
       { $project: { player: '$_id', count: 1, _id: 0 } },
