00efa9dc602b62be85ec40e4ae49090803960b08
Michał Garapich <michal@garapich.pl>
2020-12-16 02:40:07 +0100

    add migration

--- migrations/1592267655405-new-game-model.js
+++ migrations/1592267655405-new-game-model.js
@@ -17,45 +17,43 @@ const uri = `mongodb://${credentials}${process.env.MONGODB_HOST}:${process.env.M
 module.exports.up = function (next) {
   mongodb.MongoClient.connect(uri, { useUnifiedTopology: true })
     .then(client => client.db())
-    .then(db => {
-      db.collection('games')
-        .updateMany(
-          { 'slots.0.player': { $exists: false } },
-          [
-            // rename slot.playerId => slot.player (and make it ObjectId)
-            // get rid of slot.teamId, use slot.team instead
-            // https://github.com/tf2pickup-pl/server/pull/434
-            // https://github.com/tf2pickup-pl/server/pull/437
-            { $set: {
-              slots: {
-                $map: {
-                  input: '$slots',
-                  as: 'x',
-                  in: {
-                    _id: '$$x._id',
-                    player: { $toObjectId: '$$x.playerId' },
-                    team: {
-                      $cond: {
-                        if: { $eq: [ '$$x.teamId', '0' ] },
-                        then: 'red',
-                        else: 'blu'
-                      }
-                    },
-                    gameClass: '$$x.gameClass',
-                    status: '$$x.status',
-                    connectionStatus: '$$x.connectionStatus',
+    .then(db => db.collection('games')
+      .updateMany(
+        { 'slots.0.player': { $exists: false } },
+        [
+          // rename slot.playerId => slot.player (and make it ObjectId)
+          // get rid of slot.teamId, use slot.team instead
+          // https://github.com/tf2pickup-pl/server/pull/434
+          // https://github.com/tf2pickup-pl/server/pull/437
+          { $set: {
+            slots: {
+              $map: {
+                input: '$slots',
+                as: 'x',
+                in: {
+                  _id: '$$x._id',
+                  player: { $toObjectId: '$$x.playerId' },
+                  team: {
+                    $cond: {
+                      if: { $eq: [ '$$x.teamId', '0' ] },
+                      then: 'red',
+                      else: 'blu'
+                    }
                   },
+                  gameClass: '$$x.gameClass',
+                  status: '$$x.status',
+                  connectionStatus: '$$x.connectionStatus',
                 },
               },
-            } },
-            // remove obsolete fields
-            // https://github.com/tf2pickup-pl/server/pull/437
-            { $unset: ['teams', 'players'] },
-          ],
-          { multi: true }
-        )
-    })
-    .then(next);
+            },
+          } },
+          // remove obsolete fields
+          // https://github.com/tf2pickup-pl/server/pull/437
+          { $unset: ['teams', 'players'] },
+        ]
+      )
+    )
+    .then(() => next());
 }
 
 module.exports.down = function (next) {
