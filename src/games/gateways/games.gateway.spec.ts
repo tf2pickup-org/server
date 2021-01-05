62f8275338636d7212e125fc2022d9bd42f4d9db
Michał Garapich <michal@garapich.pl>
2021-01-05 10:09:54 +0100

    update tests

--- src/games/gateways/games.gateway.spec.ts
+++ src/games/gateways/games.gateway.spec.ts
@@ -2,26 +2,29 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GamesGateway } from './games.gateway';
 import { PlayerSubstitutionService } from '../services/player-substitution.service';
 import { Events } from '@/events/events';
+import { Game } from '../models/game';
+import { DocumentType } from '@typegoose/typegoose';
+import { Socket } from 'socket.io';
 
-const game = {
+jest.mock('../services/player-substitution.service');
+jest.mock('socket.io');
+
+const mockGame = {
   id: 'FAKE_GAME_ID',
   state: 'launching',
-};
-
-class PlayerSubstitutionServiceStub {
-  replacePlayer(gameId: string, replaceeId: string, replacementId: string) { return new Promise(resolve => resolve(game)); }
-}
+} as DocumentType<Game>;
 
 describe('GamesGateway', () => {
   let gateway: GamesGateway;
-  let playerSubstitutionService: PlayerSubstitutionServiceStub;
+  let playerSubstitutionService: jest.Mocked<PlayerSubstitutionService>;
   let events: Events;
+  let socket: jest.Mocked<Socket>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         GamesGateway,
-        { provide: PlayerSubstitutionService, useClass: PlayerSubstitutionServiceStub },
+        PlayerSubstitutionService,
         Events,
       ],
     }).compile();
@@ -31,16 +34,47 @@ describe('GamesGateway', () => {
     events = module.get(Events);
   });
 
+  beforeEach(() => {
+    playerSubstitutionService.replacePlayer.mockResolvedValue(mockGame);
+    socket = {
+      emit: jest.fn(),
+    } as any;
+  });
+
+  beforeEach(() => {
+    gateway.onModuleInit();
+    gateway.afterInit(socket);
+  });
+
   it('should be defined', () => {
     expect(gateway).toBeDefined();
   });
 
   describe('#replacePlayer()', () => {
     it('should replace the player', async () => {
-      const spy = jest.spyOn(playerSubstitutionService, 'replacePlayer');
       const ret = await gateway.replacePlayer({ request: { user: { id: 'FAKE_REPLACEMENT_ID' } } }, { gameId: 'FAKE_GAME_ID', replaceeId: 'FAKE_REPLACEE_ID' });
-      expect(spy).toHaveBeenCalledWith('FAKE_GAME_ID', 'FAKE_REPLACEE_ID', 'FAKE_REPLACEMENT_ID');
-      expect(ret).toEqual(game as any);
+      expect(playerSubstitutionService.replacePlayer).toHaveBeenCalledWith('FAKE_GAME_ID', 'FAKE_REPLACEE_ID', 'FAKE_REPLACEMENT_ID');
+      expect(ret).toEqual(mockGame as any);
+    });
+  });
+
+  describe('when the gameCreated event is emitted', () => {
+    beforeEach(() => {
+      events.gameCreated.next({ game: mockGame });
+    });
+
+    it('should emit the created game via the socket', () => {
+      expect(socket.emit).toHaveBeenCalledWith('game created', mockGame);
+    });
+  });
+
+  describe('when the gameChanges event is emitted', () => {
+    beforeEach(() => {
+      events.gameChanges.next({ game: mockGame });
+    });
+
+    it('should emit the created game via the socket', () => {
+      expect(socket.emit).toHaveBeenCalledWith('game updated', mockGame);
     });
   });
 });
