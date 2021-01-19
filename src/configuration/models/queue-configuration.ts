6d318ed4617dd94c831b3ab91d968dffc7cb7e81
Michał Garapich <michal@garapich.pl>
2021-01-19 23:48:53 +0100

    get rid of configuration module

--- src/configuration/models/queue-configuration.ts
+++
@@ -1,9 +0,0 @@
-import { prop } from '@typegoose/typegoose';
-import { Map } from './map';
-
-export class QueueConfiguration {
-
-  @prop({ type: () => [Map], required: true })
-  maps!: Map[];
-
-}
