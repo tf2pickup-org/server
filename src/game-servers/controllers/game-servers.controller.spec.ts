d795b18c9903b2a9c0a7dc5603f23178a881bdad
Michał Garapich <michal@garapich.pl>
2021-03-12 20:40:00 +0100

    fix controller test

--- src/game-servers/controllers/game-servers.controller.spec.ts
+++ src/game-servers/controllers/game-servers.controller.spec.ts
@@ -4,28 +4,32 @@ import { GameServersService } from '../services/game-servers.service';
 import { GameServer } from '../models/game-server';
 import { NotFoundException } from '@nestjs/common';
 
-class GameServersServiceStub {
-  gameServer: GameServer = {
-    name: 'FAKE_NAME',
-    address: 'FAKE_ADDRESS',
-    port: '27015',
-    rconPassword: 'FAKE_RCON_PASSWORD',
-  };
+// class GameServersServiceStub {
 
-  async getAllGameServers() { return new Promise(resolve => resolve([ this.gameServer ])); }
-  async getById(id: string) { return new Promise(resolve => resolve(this.gameServer)); }
-  async addGameServer(gameServer: any) { return new Promise(resolve => resolve(gameServer)); }
-  async removeGameServer(id: string) { return new  Promise(resolve => resolve()); }
-}
+
+//   async getAllGameServers() { return new Promise(resolve => resolve([ this.gameServer ])); }
+//   async getById(id: string) { return new Promise(resolve => resolve(this.gameServer)); }
+//   async addGameServer(gameServer: any) { return new Promise(resolve => resolve(gameServer)); }
+//   async removeGameServer(id: string) { return new  Promise(resolve => resolve()); }
+// }
+
+const mockGameServer: GameServer = {
+  name: 'FAKE_NAME',
+  address: 'FAKE_ADDRESS',
+  port: '27015',
+  rconPassword: 'FAKE_RCON_PASSWORD',
+};
+
+jest.mock('../services/game-servers.service');
 
 describe('GameServers Controller', () => {
   let controller: GameServersController;
-  let gameServersService: GameServersServiceStub;
+  let gameServersService: jest.Mocked<GameServersService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
-        { provide: GameServersService, useClass: GameServersServiceStub },
+        GameServersService,
       ],
       controllers: [GameServersController],
     }).compile();
@@ -39,42 +43,50 @@ describe('GameServers Controller', () => {
   });
 
   describe('#getAllGameServers()', () => {
+    beforeEach(() => {
+      gameServersService.getAllGameServers.mockResolvedValue([ mockGameServer ]);
+    });
+
     it('should call service', async () => {
-      const spy = jest.spyOn(gameServersService, 'getAllGameServers');
       const ret = await controller.getAllGameServers();
-      expect(spy).toHaveBeenCalled();
-      expect(ret).toEqual([ gameServersService.gameServer ] as any[]);
+      expect(gameServersService.getAllGameServers).toHaveBeenCalled();
+      expect(ret).toEqual([ mockGameServer ]);
     });
   });
 
   describe('#getGameServer()', () => {
-    it('should return the game server', async () => {
-      const spy = jest.spyOn(gameServersService, 'getById');
-      const ret = await controller.getGameServer('FAKE_ID');
-      expect(spy).toHaveBeenCalledWith('FAKE_ID');
-      expect(ret).toEqual(gameServersService.gameServer as any);
+    beforeEach(() => {
+      gameServersService.getById.mockResolvedValue(mockGameServer);
     });
 
-    it('should return 404', async () => {
-      jest.spyOn(gameServersService, 'getById').mockImplementation(() => new Promise(resolve => resolve(null)));
-      await expect(controller.getGameServer('FAKE_ID')).rejects.toThrow(NotFoundException);
+    it('should return the game server', async () => {
+      const ret = await controller.getGameServer('FAKE_ID');
+      expect(gameServersService.getById).toHaveBeenCalledWith('FAKE_ID');
+      expect(ret).toEqual(mockGameServer);
     });
   });
 
   describe('#addGameServer()', () => {
+    beforeEach(() => {
+      gameServersService.addGameServer.mockResolvedValue(mockGameServer);
+    });
+
     it('should add the game server', async () => {
-      const spy = jest.spyOn(gameServersService, 'addGameServer');
-      const ret = await controller.addGameServer(gameServersService.gameServer);
-      expect(spy).toHaveBeenCalledWith(gameServersService.gameServer);
-      expect(ret).toEqual(gameServersService.gameServer as any);
+      const dto = { name: 'FAKE_SERVER_NAME', port: '27015', address: 'FAKE_SERVER_ADDRESS', rconPassword: 'FAKE_RCON_PASSWORD' };
+      const ret = await controller.addGameServer(dto);
+      expect(gameServersService.addGameServer).toHaveBeenCalledWith(dto);
+      expect(ret).toEqual(mockGameServer);
     });
   });
 
   describe('#removeGameServer()', () => {
+    beforeEach(() => {
+      gameServersService.removeGameServer.mockResolvedValue();
+    });
+
     it('should call the service', async () => {
-      const spy = jest.spyOn(gameServersService, 'removeGameServer');
       await controller.removeGameServer('FAKE_ID');
-      expect(spy).toHaveBeenCalledWith('FAKE_ID');
+      expect(gameServersService.removeGameServer).toHaveBeenCalledWith('FAKE_ID');
     });
   });
 });
