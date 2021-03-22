f9d57534305062e71a461c274d3f0b99fc06c793
Michał Garapich <michal@garapich.pl>
2021-03-22 21:30:46 +0100

    fix e2e test

--- e2e/game-server-diagnostics/game-server-diagnostics.e2e-spec.ts
+++ e2e/game-server-diagnostics/game-server-diagnostics.e2e-spec.ts
@@ -7,9 +7,12 @@ import { PlayersService } from '@/players/services/players.service';
 import { AuthService } from '@/auth/services/auth.service';
 import { JwtTokenPurpose } from '@/auth/jwt-token-purpose';
 import { GameServersService } from '@/game-servers/services/game-servers.service';
-import { wipeDatabase } from '../wipe-database';
 import { GameServerDiagnosticsService } from '@/game-servers/services/game-server-diagnostics.service';
 import { DiagnosticRunStatus } from '@/game-servers/models/diagnostic-run-status';
+import { getModelToken } from 'nestjs-typegoose';
+import { Player } from '@/players/models/player';
+import { ReturnModelType } from '@typegoose/typegoose';
+import { GameServer } from '@/game-servers/models/game-server';
 
 jest.mock('@/players/services/steam-api.service');
 jest.setTimeout(10000);
@@ -28,11 +31,6 @@ describe('Game server diagnostics (e2e)', () => {
     await app.init();
   });
 
-  afterAll(async () => {
-    await app.close();
-    await wipeDatabase();
-  });
-
   beforeAll(async () => {
     const playersService = app.get(PlayersService);
     const maly = await playersService.createPlayer((await import('../steam-profiles')).maly);
@@ -49,6 +47,16 @@ describe('Game server diagnostics (e2e)', () => {
     });
 
     gameServerId = gameServer.id;
+  });
+
+  afterAll(async () => {
+    const playerModel = app.get(getModelToken(Player.name)) as ReturnModelType<typeof Player>;
+    await playerModel.deleteMany({ });
+
+    const gameServerModel = app.get(getModelToken(GameServer.name)) as ReturnModelType<typeof GameServer>;
+    await gameServerModel.deleteMany({ });
+
+    await app.close();
   });
 
   describe('diagnostic run is requested', () => {
