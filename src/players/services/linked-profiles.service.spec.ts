d0bcf1464f469e2607b471ef92e192dd09db89e2
Michał Garapich <michal@garapich.pl>
2021-04-15 00:59:22 +0200

    add LinkedProfilesService tests

--- src/players/services/linked-profiles.service.spec.ts
+++ src/players/services/linked-profiles.service.spec.ts
@@ -15,4 +15,28 @@ describe('LinkedProfilesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
+  describe('#getLinkedProfiles()', () => {
+    beforeEach(() => {
+      service.registerLinkedProfileProvider({
+        name: 'test',
+        fetchProfile: (playerId) =>
+          Promise.resolve({
+            playerId,
+            test: 'test',
+          }),
+      });
+    });
+
+    it('should fetch the linked profile', async () => {
+      const linkedProfiles = await service.getLinkedProfiles('FAKE_PLAYER_ID');
+      expect(linkedProfiles).toEqual([
+        {
+          provider: 'test',
+          playerId: 'FAKE_PLAYER_ID',
+          test: 'test',
+        },
+      ]);
+    });
+  });
 });
