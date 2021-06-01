fa3df05f63b6cb269e825ba0d4fcf5ce4df323dd
Michał Garapich <michal@garapich.pl>
2021-06-01 02:01:32 +0200

    feat(twitch.tv): promoted streams (#1040)

---
+++ configs/twitchtv.ts
@@ -0,0 +1,13 @@
+export const promotedStreams = [
+  'teamfortresstv',
+  'teamfortresstv2',
+  'teamfortresstv3',
+  'kritzkast',
+  'kritzkast2',
+  'rglgg',
+  'essentialstf',
+  'cappingtv',
+  'cappingtv2',
+  'cappingtv3',
+  'tflivetv',
+];
