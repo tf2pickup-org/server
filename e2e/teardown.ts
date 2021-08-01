5a213240f30adf8ca7854ccd488164c0b32a4150
Michał Garapich <michal@garapich.pl>
2021-08-01 14:08:36 +0200

    add websocket e2e tests

--- e2e/teardown.ts
+++ e2e/teardown.ts
@@ -2,13 +2,16 @@ import { MongoClient } from 'mongodb';
 import { createMongoDbUri } from '../src/utils/create-mongo-db-uri';
 
 const teardown = async () => {
-  const client = await MongoClient.connect(createMongoDbUri({
-    host: process.env.MONGODB_HOST,
-    port: process.env.MONGODB_PORT,
-  }), {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+  const client = await MongoClient.connect(
+    createMongoDbUri({
+      host: process.env.MONGODB_HOST,
+      port: process.env.MONGODB_PORT,
+    }),
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+  );
 
   await client.db(process.env.MONGODB_DB).dropDatabase();
   console.log(`Dropped ${process.env.MONGODB_DB}`);
