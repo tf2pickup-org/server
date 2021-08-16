644212a63b4e1b56b09df23f66d1f19693564748
Michał Garapich <michal@garapich.pl>
2021-08-16 11:59:23 +0200

    fix(players): verbose tf2 in-game hours verification logging

--- src/players/errors/tf2-in-game-hours-verification.error.ts
+++ src/players/errors/tf2-in-game-hours-verification.error.ts
@@ -1,5 +1,5 @@
 export class Tf2InGameHoursVerificationError extends Error {
   constructor(public verificationErrorMessage: string) {
-    super(`cannot verify in-game hours for TF2`);
+    super(`cannot verify in-game hours for TF2 (${verificationErrorMessage})`);
   }
 }
