4d59edebf176ca30ef2e99752b04bbfc57868f73
Michał Garapich <michal@garapich.pl>
2021-08-23 23:02:43 +0200

    fix?

--- src/utils/testing-mongoose-module.ts
+++ src/utils/testing-mongoose-module.ts
@@ -1,16 +1,10 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 
-export const mongooseTestingModule = (mongod?: MongoMemoryServer) =>
-  MongooseModule.forRootAsync({
-    useFactory: async () => {
-      mongod = mongod ?? (await MongoMemoryServer.create());
-      return {
-        uri: mongod.getUri(),
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      };
-    },
+export const mongooseTestingModule = (mongod: MongoMemoryServer) =>
+  MongooseModule.forRoot(mongod.getUri(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
   });
