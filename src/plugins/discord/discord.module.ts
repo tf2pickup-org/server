dea1f3a7f4e36eeefa2f23ff2a22c2930cb25f6b
Michał Garapich <michal@garapich.pl>
2021-09-14 10:56:02 +0200

    fix(discord): import GamesModule (#1225)

--- src/plugins/discord/discord.module.ts
+++ src/plugins/discord/discord.module.ts
@@ -4,10 +4,15 @@ import { forwardRef, Global, Module } from '@nestjs/common';
 import { DiscordService } from './services/discord.service';
 import { QueuePromptsService } from './services/queue-prompts.service';
 import { AdminNotificationsService } from './services/admin-notifications.service';
+import { GamesModule } from '@/games/games.module';
 
 @Global()
 @Module({
-  imports: [forwardRef(() => QueueModule), forwardRef(() => PlayersModule)],
+  imports: [
+    forwardRef(() => QueueModule),
+    forwardRef(() => PlayersModule),
+    GamesModule,
+  ],
   providers: [DiscordService, QueuePromptsService, AdminNotificationsService],
   exports: [DiscordService],
 })
