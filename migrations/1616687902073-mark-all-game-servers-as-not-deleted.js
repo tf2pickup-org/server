60d25f59f42c347115e6b4ba46da96952db7a7d6
Michał Garapich <michal@garapich.pl>
2021-10-10 21:33:02 +0200

    fix(environment): use MONGODB_URI env

--- migrations/1616687902073-mark-all-game-servers-as-not-deleted.js
+++ migrations/1616687902073-mark-all-game-servers-as-not-deleted.js
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-'use strict'
+'use strict';
 
 //
 // Set deleted to false on all game servers that do not have the 'deleted' property.
@@ -8,30 +8,21 @@
 const { config } = require('dotenv');
 const { MongoClient } = require('mongodb');
 
-module.exports.up = next => {
+module.exports.up = (next) => {
   config();
 
-  let credentials = '';
-  if (process.env.MONGODB_USERNAME) {
-    if (process.env.MONGODB_PASSWORD) {
-      credentials = `${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@`;
-    } else {
-      credentials = `${process.env.MONGODB_USERNAME}@`;
-    }
-  }
-
-  const uri = `mongodb://${credentials}${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
-
-  MongoClient.connect(uri, { useUnifiedTopology: true })
-    .then(client => client.db())
-    .then(db => db.collection('gameservers'))
-    .then(collection => collection.updateMany(
-      {
-        deleted: { $exists: false },
-      },
-      {
-        $set: { deleted: false },
-      },
-    ))
+  MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+    .then((client) => client.db())
+    .then((db) => db.collection('gameservers'))
+    .then((collection) =>
+      collection.updateMany(
+        {
+          deleted: { $exists: false },
+        },
+        {
+          $set: { deleted: false },
+        },
+      ),
+    )
     .then(() => next());
-}
+};
--- migrations/1628156071212-mumble-server-to-configuration.js
+++ migrations/1628156071212-mumble-server-to-configuration.js
@@ -7,23 +7,12 @@ const { MongoClient } = require('mongodb');
 module.exports.up = (next) => {
   config();
 
-  let credentials = '';
-  if (process.env.MONGODB_USERNAME) {
-    if (process.env.MONGODB_PASSWORD) {
-      credentials = `${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@`;
-    } else {
-      credentials = `${process.env.MONGODB_USERNAME}@`;
-    }
-  }
-
-  const uri = `mongodb://${credentials}${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DB}`;
-
   if (process.env.MUMBLE_SERVER_URL && process.env.MUMBLE_CHANNEL_NAME) {
     const url = process.env.MUMBLE_SERVER_URL;
     const port = 64738;
     const channelName = process.env.MUMBLE_CHANNEL_NAME;
 
-    MongoClient.connect(uri, { useUnifiedTopology: true })
+    MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
       .then((client) => client.db())
       .then((db) => db.collection('configuration'))
       .then((collection) =>
