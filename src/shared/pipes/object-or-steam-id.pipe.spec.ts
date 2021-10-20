ad143a5f8a31bc4aa5001393cc5fd92c6111c15f
c43721 <rlahman.game@gmail.com>
2021-10-19 19:14:36 -0500

    feat: add tests for pipe, fix pipe throwing wrong error type

---
+++ src/shared/pipes/object-or-steam-id.pipe.spec.ts
@@ -0,0 +1,36 @@
+import { ObjectOrSteamIdPipe } from './object-or-steam-id.pipe';
+import { ObjectId } from 'mongodb';
+import { BadRequestException } from '@nestjs/common';
+
+describe('ObjectOrSteamIdPipe', () => {
+  it('should be defined', () => {
+    expect(new ObjectOrSteamIdPipe()).toBeDefined();
+  });
+
+  describe('object id', () => {
+    it('should pass valid object id', () => {
+        const id = new ObjectId().toString();
+        expect(new ObjectOrSteamIdPipe().transform(id)).toEqual(id);
+      });
+    
+      it('should deny invalid object id', () => {
+        expect(() =>
+          new ObjectOrSteamIdPipe().transform('some invalid object id'),
+        ).toThrow(BadRequestException);
+      });
+  })
+
+  describe('steam id', () => {
+    it('should pass valid steam id', () => {
+        const id = "76561198074409147";
+
+        expect(new ObjectOrSteamIdPipe().transform(id)).toEqual(id);
+      });
+    
+      it('should deny invalid object id', () => {
+        expect(() =>
+          new ObjectOrSteamIdPipe().transform('some invalid steam id'),
+        ).toThrow(BadRequestException);
+      });
+  })
+});
--- src/shared/pipes/object-or-steam-id.pipe.ts
+++ src/shared/pipes/object-or-steam-id.pipe.ts
@@ -1,6 +1,6 @@
 import { Injectable, PipeTransform, BadRequestException } from '@nestjs/common';
 import { Types } from 'mongoose';
-import SteamID from 'steamid';
+import * as SteamID from 'steamid';
 
 /**
  * Throws BadRequestException if the provided value is not a valid ObjectId OR SteamId.
@@ -12,10 +12,14 @@ import SteamID from 'steamid';
 @Injectable()
 export class ObjectOrSteamIdPipe implements PipeTransform {
   transform(value: string) {
-    if (Types.ObjectId.isValid(value) || new SteamID(value).isValid()) {
-      return value;
+    try {
+      if (Types.ObjectId.isValid(value) || new SteamID(value).isValid()) {
+        return value;
+      }
+    } catch {
+      // don't care about error
+      // needed because "new SteamID" throws a general "Error", but we want a "BadRequestException"
+      throw new BadRequestException('value is not a valid objectid or steamid');
     }
-
-    throw new BadRequestException("value is not a valid objectid or steamid");
   }
 }
