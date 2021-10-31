fbcf6f8395143bf134652cd414f00ffe80bb115c
Michał Garapich <michal@garapich.pl>
2021-10-31 23:43:40 +0100

    add migration

---
+++ migrations/1635720162156-add-game-servers-priority.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict';
+
+const { config } = require('dotenv');
+const { MongoClient } = require('mongodb');
+
+module.exports.up = (next) => {
+  config();
+
+  MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+    .then((client) => client.db())
+    .then((db) => db.collection('gameservers'))
+    .then((collection) =>
+      Promise.all([
+        collection,
+        collection.updateMany({}, { $set: { priority: 1 } }),
+      ]),
+    )
+    .then(() => next());
+};
