485c8ac1b0a362c15fae02720747b46bd310d1f2
Michał Garapich <michal@garapich.pl>
2022-03-10 01:22:18 +0100

    tests

--- src/game-servers/providers/static-game-server/models/static-game-server.spec.ts
+++ src/game-servers/providers/static-game-server/models/static-game-server.spec.ts
@@ -1,4 +1,26 @@
+import { createRcon } from '@/utils/create-rcon';
+import { Rcon } from 'rcon-client';
 import { StaticGameServer } from './static-game-server';
+
+jest.mock('../utils/generate-logsecret', () => ({
+  generateLogsecret: jest.fn().mockReturnValue('FAKE_LOGSECRET'),
+}));
+
+jest.mock('rcon-client', () => ({
+  Rcon: jest.fn().mockImplementation(function () {
+    return {
+      send: jest.fn().mockResolvedValue(this),
+      end: jest.fn().mockResolvedValue(this),
+    };
+  }),
+}));
+
+jest.mock('@/utils/create-rcon');
+
+class RconStub {
+  send = jest.fn();
+  end = jest.fn();
+}
 
 describe('StaticGameServer', () => {
   describe('#voiceChannelName', () => {
@@ -29,6 +51,30 @@ describe('StaticGameServer', () => {
           'test_voice_channel',
         );
       });
+    });
+  });
+
+  describe('#getLogsecret()', () => {
+    let gameServer: StaticGameServer;
+    let rcon: RconStub;
+
+    beforeEach(() => {
+      gameServer = new StaticGameServer();
+      rcon = new RconStub();
+
+      (createRcon as jest.MockedFunction<typeof createRcon>).mockResolvedValue(
+        rcon as unknown as Rcon,
+      );
+    });
+
+    it('should generate a logsecret', async () => {
+      const logSecret = await gameServer.getLogsecret();
+      expect(logSecret).toEqual('FAKE_LOGSECRET');
+    });
+
+    it('should set the logsecret on the gameserver', async () => {
+      await gameServer.getLogsecret();
+      expect(rcon.send).toHaveBeenCalledWith('sv_logsecret FAKE_LOGSECRET');
     });
   });
 });
