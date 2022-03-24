97dacd75954bd8a76b4a4ff33fab5d33c936b44f
Michał Garapich <michal@garapich.pl>
2022-03-24 21:14:31 +0100

    cleanup

--- src/game-servers/providers/game-servers-providers.module.ts
+++ src/game-servers/providers/game-servers-providers.module.ts
@@ -1,21 +1,23 @@
 import { DynamicModule, Module } from '@nestjs/common';
 import { StaticGameServerModule } from './static-game-server/static-game-server.module';
 import { ServemeTfModule } from './serveme-tf/serveme-tf.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({})
 export class GameServersProvidersModule {
-  static configure(): DynamicModule {
-    const enabledProviderModules = [StaticGameServerModule];
+  static async configure(): Promise<DynamicModule> {
+    await ConfigModule.envVariablesLoaded;
 
-    // console.log(process.env);
-    // if (process.env.SERVEME_TF_API_KEY) {
-      enabledProviderModules.push(ServemeTfModule);
-    // }
+    const enabledProviders = [StaticGameServerModule];
+
+    if (process.env.SERVEME_TF_API_KEY) {
+      enabledProviders.push(ServemeTfModule);
+    }
 
     return {
       module: GameServersProvidersModule,
-      imports: enabledProviderModules,
-      exports: enabledProviderModules,
+      imports: [...enabledProviders],
+      exports: [...enabledProviders],
     };
   }
 }
