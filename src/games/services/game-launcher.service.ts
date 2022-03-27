43848e617cf7642a1e85baaabe442fb36e682a67
Michał Garapich <michal@garapich.pl>
2022-03-27 20:03:53 +0200

    fix

--- src/games/services/game-launcher.service.ts
+++ src/games/services/game-launcher.service.ts
@@ -31,49 +31,7 @@ export class GameLauncherService {
    */
   async launch(gameId: string): Promise<Game> {
     return await this.mutex.runExclusive(async () => {
-      let game = await this.gamesService.getById(gameId);
-
-      if (!game.isInProgress()) {
-        this.logger.warn(
-          `trying to launch game #${game.number} that has already been ended`,
-        );
-      }
-
-      try {
-        // step 1: obtain a free server
-        const gameServer = await this.gameServersService.assignGameServer(
-          game.id,
-        );
-        this.logger.verbose(
-          `using server ${gameServer.name} for game #${game.number}`,
-        );
-
-        // step 2: obtain logsecret
-        const logSecret = await gameServer.getLogsecret();
-        game = await this.gamesService.update(game.id, { logSecret });
-        this.logger.debug(
-          `[${gameServer.name}] logsecret is ${game.logSecret}`,
-        );
-
-        // step 3: configure server
-        const { connectString, stvConnectString } =
-          await this.serverConfiguratorService.configureServer(game.id);
-
-        game = await this.gamesService.update(game.id, {
-          $set: {
-            connectString,
-            stvConnectString,
-          },
-          $inc: {
-            connectInfoVersion: 1,
-          },
-        });
-
-        this.logger.verbose(`game #${game.number} initialized`);
-        return game;
-      } catch (error) {
-        this.logger.error(`Error launching game #${game.number}: ${error}`);
-      }
+      return await this.doLaunch(gameId);
     });
   }
 
@@ -83,8 +41,52 @@ export class GameLauncherService {
       const orphanedGames = await this.gamesService.getOrphanedGames();
       for (const game of orphanedGames) {
         this.logger.verbose(`launching game #${game.number}...`);
-        await this.launch(game.id);
+        await this.doLaunch(game.id);
       }
     });
+  }
+
+  private async doLaunch(gameId: string): Promise<Game> {
+    let game = await this.gamesService.getById(gameId);
+
+    if (!game.isInProgress()) {
+      this.logger.warn(
+        `trying to launch game #${game.number} that has already been ended`,
+      );
+    }
+
+    try {
+      // step 1: obtain a free server
+      const gameServer = await this.gameServersService.assignGameServer(
+        game.id,
+      );
+      this.logger.verbose(
+        `using server ${gameServer.name} for game #${game.number}`,
+      );
+
+      // step 2: obtain logsecret
+      const logSecret = await gameServer.getLogsecret();
+      game = await this.gamesService.update(game.id, { logSecret });
+      this.logger.debug(`[${gameServer.name}] logsecret is ${game.logSecret}`);
+
+      // step 3: configure server
+      const { connectString, stvConnectString } =
+        await this.serverConfiguratorService.configureServer(game.id);
+
+      game = await this.gamesService.update(game.id, {
+        $set: {
+          connectString,
+          stvConnectString,
+        },
+        $inc: {
+          connectInfoVersion: 1,
+        },
+      });
+
+      this.logger.verbose(`game #${game.number} initialized`);
+      return game;
+    } catch (error) {
+      this.logger.error(`Error launching game #${game.number}: ${error}`);
+    }
   }
 }
