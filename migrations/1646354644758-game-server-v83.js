cbe886d3838d3a9985125af857668d17bed6d6d9
Michał Garapich <michal@garapich.pl>
2022-05-16 19:33:04 +0200

    fix(game-servers): fix v9 database migration (#1613)

--- migrations/1646354644758-game-server-v83.js
+++ migrations/1646354644758-game-server-v83.js
@@ -1,26 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 'use strict';
 
-const { config } = require('dotenv');
-const { MongoClient } = require('mongodb');
-
-module.exports.up = (next) => {
-  config();
-
-  MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
-    .then((client) => client.db())
-    .then((db) => db.collection('gameservers'))
-    .then((collection) =>
-      Promise.all([
-        collection,
-        collection.updateMany({}, { $set: { provider: 'static' } }),
-      ]),
-    )
-    .then(([collection]) =>
-      Promise.all([
-        collection,
-        collection.updateMany({}, { $unset: { voiceChannelName: 1 } }),
-      ]),
-    )
-    .then(() => next());
+module.exports.up = () => {
+  // empty on purpose
 };
---
+++ migrations/1652720907471-game-server-v9.js
@@ -0,0 +1,38 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+'use strict';
+
+const { config } = require('dotenv');
+const { MongoClient } = require('mongodb');
+
+module.exports.up = (next) => {
+  config();
+
+  MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+    .then((client) => client.db())
+    .then((db) => db.collection('gameservers'))
+    .then((collection) =>
+      Promise.all([
+        collection,
+        collection.updateMany(
+          { provider: { $exists: false } },
+          { $set: { provider: 'static', isClean: true } },
+        ),
+      ]),
+    )
+    .then(([collection]) =>
+      Promise.all([
+        collection,
+        collection.updateMany(
+          {},
+          {
+            $unset: {
+              voiceChannelName: 1,
+              resolvedIpAddresses: 1,
+              isAvailable: 1,
+            },
+          },
+        ),
+      ]),
+    )
+    .then(() => next());
+};
