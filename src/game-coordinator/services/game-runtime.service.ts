a46b12e88cb6c94bf5703654711cf44d78f9ab3f
Michał Garapich <michal@garapich.pl>
2022-08-07 12:52:10 +0200

    fix(game-coordinator): bring back reinitialize server

--- src/game-coordinator/services/game-runtime.service.ts
+++ src/game-coordinator/services/game-runtime.service.ts
@@ -36,6 +36,9 @@ export class GameRuntimeService implements OnModuleInit {
       async ({ gameId, replaceeId, replacementId }) =>
         await this.replacePlayer(gameId, replaceeId, replacementId),
     );
+    this.events.gameReconfigureRequested.subscribe(
+      async ({ gameId }) => await this.reconfigure(gameId),
+    );
   }
 
   async reconfigure(gameId: string) {
