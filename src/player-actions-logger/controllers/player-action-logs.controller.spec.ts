462528b226d40faf847c8350cb88ba6a785c6cee
Michał Garapich <michal@garapich.pl>
2022-11-06 03:03:40 +0100

    test

--- src/player-actions-logger/controllers/player-action-logs.controller.spec.ts
+++ src/player-actions-logger/controllers/player-action-logs.controller.spec.ts
@@ -1,15 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PlayerActionsRepositoryService } from '../services/player-actions-repository.service';
 import { PlayerActionLogsController } from './player-action-logs.controller';
+
+jest.mock('../services/player-actions-repository.service');
 
 describe('PlayerActionLogsController', () => {
   let controller: PlayerActionLogsController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [PlayerActionLogsController],
+      controllers: [PlayerActionLogsController, PlayerActionsRepositoryService],
     }).compile();
 
-    controller = module.get<PlayerActionLogsController>(PlayerActionLogsController);
+    controller = module.get<PlayerActionLogsController>(
+      PlayerActionLogsController,
+    );
   });
 
   it('should be defined', () => {
