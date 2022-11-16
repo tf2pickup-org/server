5b0fc15a40ace4d1b617354866a3f20409e4de2b
Michał Garapich <michal@garapich.pl>
2022-11-16 01:55:12 +0100

    initial

--- src/queue-config/interfaces/queue-config.ts
+++ src/queue-config/interfaces/queue-config.ts
@@ -1,9 +1,19 @@
 import { GameClass } from './game-class';
 
 export interface QueueConfig {
-  /* This is always 2 */
+  // This is always 2
   teamCount: 2;
 
-  /* List of classes that play the given gamemode */
+  // List of classes that play the given gamemode
   classes: GameClass[];
+
+  // Time players have to ready up before they are kicked out of the queue [ms]
+  readyUpTimeout: number;
+
+  // Time the queue stays in ready-up state before going back to the 'waiting' state,
+  // unless all players ready up [ms]
+  readyStateTimeout: number;
+
+  // How many times the last played map cannot be an option to vote for
+  mapCooldown: number;
 }
