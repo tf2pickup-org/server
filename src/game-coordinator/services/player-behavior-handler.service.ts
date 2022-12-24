b23a3c1a8a52f41ebcc925f9ee2af0b0196b98a7
Michał Garapich <michal@garapich.pl>
2022-12-24 03:19:37 +0100

    devel

--- src/game-coordinator/services/player-behavior-handler.service.ts
+++ src/game-coordinator/services/player-behavior-handler.service.ts
@@ -1,51 +1,50 @@
-import { Events } from '@/events/events';
-import { Game } from '@/games/models/game';
+import { GameState } from '@/games/models/game-state';
 import { PlayerConnectionStatus } from '@/games/models/player-connection-status';
 import { GamesService } from '@/games/services/games.service';
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
-import { filter } from 'rxjs';
+import { PlayerSubstitutionService } from '@/games/services/player-substitution.service';
+import { PlayersService } from '@/players/services/players.service';
+import { Injectable, Logger } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
-export class PlayerBehaviorHandlerService implements OnModuleInit {
+export class PlayerBehaviorHandlerService {
   private logger = new Logger(PlayerBehaviorHandlerService.name);
-  // game id <-> timeout pairs
-  private timeouts = new Map<string, NodeJS.Timeout>();
 
   constructor(
-    private readonly events: Events,
     private readonly gamesService: GamesService,
+    private readonly playerSubstitutionService: PlayerSubstitutionService,
+    private readonly playersService: PlayersService,
   ) {}
 
-  onModuleInit() {
-    this.events.gameChanges
-      .pipe(
-        filter(
-          ({ oldGame, newGame }) =>
-            oldGame.connectInfoVersion !== newGame.connectInfoVersion,
-        ),
-      )
-      .subscribe(
-        async ({ newGame }) => await this.verifyPlayersConnected(newGame),
-      );
-  }
-
-  async verifyPlayersConnected(game: Game) {
-    if (!game.connectString) {
-      this.timeouts.delete(game.id);
-      return;
-    }
-
-    const timeout = setTimeout(async () => {
-      const nowGame = await this.gamesService.getById(game.id);
-      nowGame
-        .activeSlots()
-        .filter(
-          (slot) => slot.connectionStatus === PlayerConnectionStatus.offline,
-        )
-        .forEach((slot) =>
-          this.logger.log(`player ${slot.player} has not joined on time`),
-        );
-    }, 60 * 1000);
-    this.timeouts.set(game.id, timeout);
+  @Cron(CronExpression.EVERY_30_SECONDS)
+  async verifyPlayersJoinedGameServer() {
+    const bot = await this.playersService.findBot();
+    const gamesLive = await this.gamesService.getRunningGames();
+    await Promise.all(
+      gamesLive
+        .filter((game) => game.state === GameState.launching)
+        .filter((game) => Boolean(game.lastConfiguredAt))
+        .filter((game) => game.launchedAt.getTime() + 1000 * 60 < Date.now())
+        .map(async (game) => {
+          return await Promise.all(
+            game
+              .activeSlots()
+              .filter(
+                (slot) =>
+                  slot.connectionStatus === PlayerConnectionStatus.offline,
+              )
+              .map(async (slot) => {
+                this.logger.warn(
+                  `player ${slot.player} has not joined the game on time`,
+                );
+                return await this.playerSubstitutionService.substitutePlayer(
+                  game.id,
+                  slot.player.toString(),
+                  bot.id,
+                );
+              }),
+          );
+        }),
+    );
   }
 }
