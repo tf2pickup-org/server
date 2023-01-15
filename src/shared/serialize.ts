13affc343673cd83f4b7ba4f01d14b6069c6f65f
Michał Garapich <michal@garapich.pl>
2023-01-15 14:11:24 +0100

    tests

--- src/shared/serialize.ts
+++ src/shared/serialize.ts
@@ -4,34 +4,55 @@ interface StringIndex {
   [key: string]: any;
 }
 
-export const serialize = async <T extends StringIndex>(
-  object: T,
+const serializeSerializable = async <
+  K extends StringIndex,
+  T extends Serializable<K>,
+>(
+  value: T,
 ): Promise<any> => {
-  if (typeof object !== 'object' || object === null) {
-    return object;
+  const ret = await value.serialize();
+  for (const key in ret) {
+    ret[key] = await serialize(ret[key]);
   }
 
-  if (object instanceof Serializable<T>) {
-    const s = await object.serialize();
-    for (const key in s) {
-      s[key] = await serialize(s[key]);
-    }
+  return ret;
+};
 
-    return s;
-  }
-
-  if (Array.isArray(object)) {
-    return await Promise.all(
-      object.map(async (element) => await serialize(element)),
-    );
-  }
-
+const serializeObject = async <T extends StringIndex>(
+  value: T,
+): Promise<any> => {
   const ret: Record<string, T[keyof T]> = {};
   await Promise.all(
-    Object.keys(object).map(
-      async (key) => (ret[key] = await serialize(object[key])),
+    Object.keys(value).map(
+      async (key) => (ret[key] = await serialize(value[key])),
     ),
   );
 
   return ret;
+};
+
+const serializeArray = async <T extends StringIndex>(
+  value: T[],
+): Promise<any> => {
+  return await Promise.all(
+    value.map(async (element) => await serialize(element)),
+  );
+};
+
+export const serialize = async <T extends StringIndex>(
+  object: T,
+): Promise<T> => {
+  if (typeof object !== 'object' || object === null) {
+    return object;
+  }
+
+  if (object instanceof Serializable) {
+    return await serializeSerializable(object);
+  }
+
+  if (Array.isArray(object)) {
+    return await serializeArray(object);
+  }
+
+  return await serializeObject(object);
 };
