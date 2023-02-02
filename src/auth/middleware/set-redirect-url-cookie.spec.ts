65f49db9d856bcb56eea13f7cba07230d45a9764
Michał Garapich <michal@garapich.pl>
2023-02-02 12:30:26 +0100

    fix test

--- src/auth/middleware/set-redirect-url-cookie.spec.ts
+++ src/auth/middleware/set-redirect-url-cookie.spec.ts
@@ -1,8 +1,9 @@
-import { NextFunction, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { setRedirectUrlCookie } from './set-redirect-url-cookie';
 
 describe('setRedirectUrlCookie', () => {
   let nextFn: NextFunction;
+  let request: Partial<Request>;
   let response: Partial<Response>;
 
   beforeEach(() => {
@@ -10,19 +11,23 @@ describe('setRedirectUrlCookie', () => {
     response = {
       cookie: jest.fn(),
     };
+    request = {
+      get: jest.fn().mockImplementation(
+        (key: string) =>
+          ({
+            referer: 'FAKE_URL',
+          }[key]),
+      ),
+    };
   });
 
   it('should set the cookie', () => {
-    setRedirectUrlCookie(
-      { query: { url: 'FAKE_URL' } } as any,
-      response as any,
-      nextFn,
-    );
+    setRedirectUrlCookie(request as Request, response as Response, nextFn);
     expect(response.cookie).toHaveBeenCalledWith('redirect-url', 'FAKE_URL');
   });
 
   it('should call the next function', () => {
-    setRedirectUrlCookie({ query: {} } as any, response as any, nextFn);
+    setRedirectUrlCookie(request as Request, response as Response, nextFn);
     expect(nextFn).toHaveBeenCalled();
   });
 });
