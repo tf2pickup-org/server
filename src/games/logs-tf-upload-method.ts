36817bcdf5cb82f21e3c3984b6e28f2bc2b3aa81
Michał Garapich <michal@garapich.pl>
2023-02-03 02:53:01 +0100

    cleanup

--- src/games/logs-tf-upload-method.ts
+++ src/games/logs-tf-upload-method.ts
@@ -1,5 +1,5 @@
 export enum LogsTfUploadMethod {
-  Off, // logs won't be uploaded at all
-  Backend, // logs will be uploaded only by the backend
-  Gameserver, // logs will be uploaded only by the gameserver
+  Off = 'off', // logs won't be uploaded at all
+  Backend = 'backend', // logs will be uploaded only by the backend
+  Gameserver = 'gameserver', // logs will be uploaded only by the gameserver
 }
