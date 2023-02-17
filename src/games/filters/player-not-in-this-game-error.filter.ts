5e8d711c579756250bb1ededfb079c8eaefc70c3
Michał Garapich <michal@garapich.pl>
2023-02-17 13:43:18 +0100

    fix(games): handle getConnectInfo() errors gracefully

---
+++ src/games/filters/player-not-in-this-game-error.filter.ts
@@ -0,0 +1,20 @@
+import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { PlayerNotInThisGameError } from '../errors/player-not-in-this-game.error';
+
+@Catch(PlayerNotInThisGameError)
+export class PlayerNotInThisGameErrorFilter
+  implements ExceptionFilter<PlayerNotInThisGameError>
+{
+  async catch(exception: PlayerNotInThisGameError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    response.status(401).json({
+      statusCode: 401,
+      path: request.url,
+      error: `you do not take part in this game`,
+    });
+  }
+}
