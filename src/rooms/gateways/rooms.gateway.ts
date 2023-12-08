01522a3c4cabb42785d923e2a10e80d8e259b40c
Michał Garapich <michal@garapich.pl>
2023-12-08 13:46:18 +0100

    add schema validation

--- src/rooms/gateways/rooms.gateway.ts
+++ src/rooms/gateways/rooms.gateway.ts
@@ -1,16 +1,34 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { ZodPipe } from '@/shared/pipes/zod.pipe';
+import {
+  ConnectedSocket,
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { Socket } from 'socket.io';
+import { z } from 'zod';
+
+const joinRoomSchema = z.string();
+const leaveRoomSchema = z.string();
 
 @WebSocketGateway()
 export class RoomsGateway {
   @SubscribeMessage('join')
-  join(client: Socket, room: string): string[] {
+  join(
+    @ConnectedSocket() client: Socket,
+    @MessageBody(new ZodPipe(joinRoomSchema))
+    room: z.infer<typeof joinRoomSchema>,
+  ): string[] {
     client.join(room);
     return Array.from(client.rooms);
   }
 
   @SubscribeMessage('leave')
-  leave(client: Socket, room: string): string[] {
+  leave(
+    @ConnectedSocket() client: Socket,
+    @MessageBody(new ZodPipe(leaveRoomSchema))
+    room: z.infer<typeof leaveRoomSchema>,
+  ): string[] {
     client.leave(room);
     return Array.from(client.rooms);
   }
