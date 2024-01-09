34d7da19147dada3448a1b895b386249db17b4c7
Michał Garapich <michal@garapich.pl>
2024-01-09 15:56:43 +0100

    remove @TransformObjectId()

--- src/logs-tf/models/game-logs.ts
+++ src/logs-tf/models/game-logs.ts
@@ -1,10 +1,8 @@
-import { TransformObjectId } from '@/shared/decorators/transform-object-id';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
 @Schema()
 export class GameLogs {
-  @TransformObjectId()
   @Prop({ required: true, unique: true, sparse: true })
   logSecret!: string;
 
