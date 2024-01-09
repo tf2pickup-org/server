17b8427736b6b298844740adc6b7b696f12d7fab
Michał Garapich <michal@garapich.pl>
2024-01-09 12:18:57 +0100

    fix test

--- src/game-servers/providers/serveme-tf/controllers/serveme-tf.controller.spec.ts
+++ src/game-servers/providers/serveme-tf/controllers/serveme-tf.controller.spec.ts
@@ -1,21 +1,27 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { ServemeTfApiService } from '../services/serveme-tf-api.service';
 import { ServemeTfController } from './serveme-tf.controller';
-
-jest.mock('../services/serveme-tf-api.service');
+import { Client, ServerId } from '@tf2pickup-org/serveme-tf-client';
+import { SERVEME_TF_CLIENT } from '../serveme-tf-client.token';
 
 describe('ServemeTfController', () => {
   let controller: ServemeTfController;
-  let servemeTfApiService: jest.Mocked<ServemeTfApiService>;
+  let servemeTfClient: jest.Mocked<Client>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ServemeTfController],
-      providers: [ServemeTfApiService],
+      providers: [
+        {
+          provide: SERVEME_TF_CLIENT,
+          useValue: {
+            findOptions: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ServemeTfController>(ServemeTfController);
-    servemeTfApiService = module.get(ServemeTfApiService);
+    servemeTfClient = module.get(SERVEME_TF_CLIENT);
   });
 
   it('should be defined', () => {
@@ -30,36 +36,40 @@ describe('ServemeTfController', () => {
 
   describe('#listAllServers()', () => {
     beforeEach(() => {
-      servemeTfApiService.listServers.mockResolvedValue([
-        {
-          id: 9118,
-          name: 'NewBrigade #1',
-          flag: 'de',
-          ip: 'new.fakkelbrigade.eu',
-          port: '27015',
-          ip_and_port: 'new.fakkelbrigade.eu:27015',
-          sdr: false,
-          latitude: 51.2993,
-          longitude: 9.491,
-        },
-        {
-          id: 9128,
-          name: 'NewBrigade #11',
-          flag: 'de',
-          ip: 'new.fakkelbrigade.eu',
-          port: '27115',
-          ip_and_port: 'new.fakkelbrigade.eu:27115',
-          sdr: false,
-          latitude: 51.2993,
-          longitude: 9.491,
-        },
-      ]);
+      servemeTfClient.findOptions.mockResolvedValue({
+        servers: [
+          {
+            id: 9118 as ServerId,
+            name: 'NewBrigade #1',
+            flag: 'de',
+            ip: 'new.fakkelbrigade.eu',
+            port: '27015',
+            ip_and_port: 'new.fakkelbrigade.eu:27015',
+            sdr: false,
+            latitude: 51.2993,
+            longitude: 9.491,
+          },
+          {
+            id: 9128 as ServerId,
+            name: 'NewBrigade #11',
+            flag: 'de',
+            ip: 'new.fakkelbrigade.eu',
+            port: '27115',
+            ip_and_port: 'new.fakkelbrigade.eu:27115',
+            sdr: false,
+            latitude: 51.2993,
+            longitude: 9.491,
+          },
+        ],
+        serverConfigs: [],
+        whitelists: [],
+      });
     });
 
     it('should return all available servers', async () => {
       const ret = await controller.listAllServers();
       expect(ret.length).toBe(2);
-      expect(servemeTfApiService.listServers).toHaveBeenCalledTimes(1);
+      expect(servemeTfClient.findOptions).toHaveBeenCalledTimes(1);
     });
   });
 });
