00f36fc0ca6f98fb6778d74c6cdebb17f071cebe
Michał Garapich <michal@garapich.pl>
2024-01-12 19:22:09 +0100

    fix migration

--- migrations/1705011828390-remove-serveme-tf-reservations-collection.js
+++ migrations/1705011828390-remove-serveme-tf-reservations-collection.js
@@ -1,14 +1,35 @@
 'use strict';
 
 const { config } = require('dotenv');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 
 module.exports.up = async function (next) {
   config();
 
   const mongo = await MongoClient.connect(process.env.MONGODB_URI);
-  const collection = mongo.db().collection('servemetfreservations');
-  await collection.drop();
+  const gamesWithServemeTfReservation = await mongo
+    .db()
+    .collection('games')
+    .find({ 'gameServer.provider': 'serveme.tf' })
+    .toArray();
 
+  for (const game of gamesWithServemeTfReservation) {
+    const reservation = await mongo
+      .db()
+      .collection('servemetfreservations')
+      .findOne({ _id: new ObjectId(game.gameServer.id) });
+
+    if (reservation) {
+      await mongo
+        .db()
+        .collection('games')
+        .updateOne(
+          { _id: game._id },
+          { $set: { 'gameServer.id': reservation.reservationId } },
+        );
+    }
+  }
+
+  await mongo.db().collection('servemetfreservations').drop();
   next();
 };
