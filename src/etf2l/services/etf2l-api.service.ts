a316f9ab43932b0611ebf62bb61f05e200220789
Michał Garapich <michal@garapich.pl>
2024-02-20 10:42:25 +0100

    fix(etf2l): replace axios with native fetch

--- src/etf2l/services/etf2l-api.service.ts
+++ src/etf2l/services/etf2l-api.service.ts
@@ -1,11 +1,8 @@
 import { etf2lApiEndpoint } from '@configs/urls';
-import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import { catchError, firstValueFrom, of, switchMap, throwError } from 'rxjs';
 import { Etf2lApiError } from '../errors/etf2l-api.error';
 import { NoEtf2lAccountError } from '../errors/no-etf2l-account.error';
 import { Etf2lProfile } from '../types/etf2l-profile';
-import { AxiosError } from 'axios';
 
 interface Etf2lPlayerResponse {
   player: Etf2lProfile;
@@ -17,39 +14,19 @@ interface Etf2lPlayerResponse {
 
 @Injectable()
 export class Etf2lApiService {
-  constructor(private readonly httpService: HttpService) {}
-
   async fetchPlayerProfile(steamIdOrEtf2lId: string): Promise<Etf2lProfile> {
     const url = `${etf2lApiEndpoint}/player/${steamIdOrEtf2lId}`;
-    return await firstValueFrom(
-      this.httpService.get<Etf2lPlayerResponse>(url).pipe(
-        catchError((err: AxiosError) => {
-          if (err.response?.status === 404) {
-            throw new NoEtf2lAccountError(steamIdOrEtf2lId);
-          }
-          throw new Etf2lApiError(url, `${err.code} ${err.message}`);
-        }),
-        switchMap((response) => {
-          switch (response.status) {
-            case 200:
-              return of(response.data.player);
 
-            case 404:
-              return throwError(
-                () => new NoEtf2lAccountError(steamIdOrEtf2lId),
-              );
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = (await response.json()) as Etf2lPlayerResponse;
+      return data.player;
+    }
 
-            default:
-              return throwError(
-                () =>
-                  new Etf2lApiError(
-                    url,
-                    `${response.status} ${response.statusText}`,
-                  ),
-              );
-          }
-        }),
-      ),
-    );
+    if (response.status === 404) {
+      throw new NoEtf2lAccountError(steamIdOrEtf2lId);
+    }
+
+    throw new Etf2lApiError(url, `${response.status} ${response.statusText}`);
   }
 }
